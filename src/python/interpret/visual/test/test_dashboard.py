83fcc2175ffa7223098e0a3b17d1be1b0bb2847d
Interpret ML <interpretml@outlook.com>
2019-06-04 19:02:43 -0700

    Add some sleep for random port test.

--- src/python/interpret/visual/test/test_dashboard.py
+++ src/python/interpret/visual/test/test_dashboard.py
@@ -2,12 +2,14 @@
 # Distributed under the MIT software license
 
 from ..dashboard import AppRunner
+from time import sleep
 
 
 def test_random_port():
     app_runner = AppRunner()
 
     app_runner.start()
+    sleep(1)
     is_alive = app_runner.ping()
     assert is_alive
     app_runner.stop()
