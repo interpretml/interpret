1f6619ee522248928d6f2575c659d24abd63a30a
Interpret ML <interpretml@outlook.com>
2019-06-08 22:05:47 -0700

    change from fPIC to fpic for linker.  fpic generates smaller/faster code

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ if [ "$os_type" = "Darwin" ]; then
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   compile_out=`$clang_pp_bin "$script_path/src/core/ebmcore/DataSetByAttribute.cpp" "$script_path/src/core/ebmcore/DataSetByAttributeCombination.cpp" "$script_path/src/core/ebmcore/InteractionDetection.cpp" "$script_path/src/core/ebmcore/Logging.cpp" "$script_path/src/core/ebmcore/SamplingWithReplacement.cpp" "$script_path/src/core/ebmcore/Training.cpp" -I"$script_path/src/core/ebmcore" -I"$script_path/src/core/inc" -std=c++11 -fpermissive -fvisibility=hidden -O3 -march=core2 -DEBMCORE_EXPORTS -fPIC -dynamiclib -m64 -DNDEBUG -install_name @rpath/ebmcore_mac_x64.dylib -o "$script_path/tmp/clang/bin/release/mac/x64/ebmcore/ebmcore_mac_x64.dylib" 2>&1`
+   compile_out=`$clang_pp_bin "$script_path/src/core/ebmcore/DataSetByAttribute.cpp" "$script_path/src/core/ebmcore/DataSetByAttributeCombination.cpp" "$script_path/src/core/ebmcore/InteractionDetection.cpp" "$script_path/src/core/ebmcore/Logging.cpp" "$script_path/src/core/ebmcore/SamplingWithReplacement.cpp" "$script_path/src/core/ebmcore/Training.cpp" -I"$script_path/src/core/ebmcore" -I"$script_path/src/core/inc" -std=c++11 -fpermissive -fvisibility=hidden -O3 -march=core2 -DEBMCORE_EXPORTS -fpic -dynamiclib -m64 -DNDEBUG -install_name @rpath/ebmcore_mac_x64.dylib -o "$script_path/tmp/clang/bin/release/mac/x64/ebmcore/ebmcore_mac_x64.dylib" 2>&1`
    ret_code=$?
    echo -n "$compile_out"
    echo -n "$compile_out" > "$script_path/tmp/clang/intermediate/release/mac/x64/ebmcore/ebmcore_release_mac_x64_build_log.txt"
@@ -59,7 +59,7 @@ if [ "$os_type" = "Darwin" ]; then
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   compile_out=`$clang_pp_bin "$script_path/src/core/ebmcore/DataSetByAttribute.cpp" "$script_path/src/core/ebmcore/DataSetByAttributeCombination.cpp" "$script_path/src/core/ebmcore/InteractionDetection.cpp" "$script_path/src/core/ebmcore/Logging.cpp" "$script_path/src/core/ebmcore/SamplingWithReplacement.cpp" "$script_path/src/core/ebmcore/Training.cpp" -I"$script_path/src/core/ebmcore" -I"$script_path/src/core/inc" -std=c++11 -fpermissive -fvisibility=hidden -O3 -march=core2 -DEBMCORE_EXPORTS -fPIC -dynamiclib -m64 -install_name @rpath/ebmcore_mac_x64_debug.dylib -o "$script_path/tmp/clang/bin/debug/mac/x64/ebmcore/ebmcore_mac_x64_debug.dylib" 2>&1`
+   compile_out=`$clang_pp_bin "$script_path/src/core/ebmcore/DataSetByAttribute.cpp" "$script_path/src/core/ebmcore/DataSetByAttributeCombination.cpp" "$script_path/src/core/ebmcore/InteractionDetection.cpp" "$script_path/src/core/ebmcore/Logging.cpp" "$script_path/src/core/ebmcore/SamplingWithReplacement.cpp" "$script_path/src/core/ebmcore/Training.cpp" -I"$script_path/src/core/ebmcore" -I"$script_path/src/core/inc" -std=c++11 -fpermissive -fvisibility=hidden -O3 -march=core2 -DEBMCORE_EXPORTS -fpic -dynamiclib -m64 -install_name @rpath/ebmcore_mac_x64_debug.dylib -o "$script_path/tmp/clang/bin/debug/mac/x64/ebmcore/ebmcore_mac_x64_debug.dylib" 2>&1`
    ret_code=$?
    echo -n "$compile_out"
    echo -n "$compile_out" > "$script_path/tmp/clang/intermediate/debug/mac/x64/ebmcore/ebmcore_debug_mac_x64_build_log.txt"
@@ -88,7 +88,7 @@ if [ "$os_type" = "Darwin" ]; then
    # if [ $ret_code -ne 0 ]; then 
    #    exit $ret_code
    # fi
-   # compile_out=`$clang_pp_bin "$script_path/src/core/ebmcore/DataSetByAttribute.cpp" "$script_path/src/core/ebmcore/DataSetByAttributeCombination.cpp" "$script_path/src/core/ebmcore/InteractionDetection.cpp" "$script_path/src/core/ebmcore/Logging.cpp" "$script_path/src/core/ebmcore/SamplingWithReplacement.cpp" "$script_path/src/core/ebmcore/Training.cpp" -I"$script_path/src/core/ebmcore" -I"$script_path/src/core/inc" -std=c++11 -fpermissive -fvisibility=hidden -O3 -march=core2 -DEBMCORE_EXPORTS -fPIC -dynamiclib -m32 -DNDEBUG -install_name @rpath/ebmcore_mac_x86.dylib -o "$script_path/tmp/clang/bin/release/mac/x86/ebmcore/ebmcore_mac_x86.dylib" 2>&1`
+   # compile_out=`$clang_pp_bin "$script_path/src/core/ebmcore/DataSetByAttribute.cpp" "$script_path/src/core/ebmcore/DataSetByAttributeCombination.cpp" "$script_path/src/core/ebmcore/InteractionDetection.cpp" "$script_path/src/core/ebmcore/Logging.cpp" "$script_path/src/core/ebmcore/SamplingWithReplacement.cpp" "$script_path/src/core/ebmcore/Training.cpp" -I"$script_path/src/core/ebmcore" -I"$script_path/src/core/inc" -std=c++11 -fpermissive -fvisibility=hidden -O3 -march=core2 -DEBMCORE_EXPORTS -fpic -dynamiclib -m32 -DNDEBUG -install_name @rpath/ebmcore_mac_x86.dylib -o "$script_path/tmp/clang/bin/release/mac/x86/ebmcore/ebmcore_mac_x86.dylib" 2>&1`
    # ret_code=$?
    # echo -n "$compile_out"
    # echo -n "$compile_out" > "$script_path/tmp/clang/intermediate/release/mac/x86/ebmcore/ebmcore_release_mac_x86_build_log.txt"
@@ -117,7 +117,7 @@ if [ "$os_type" = "Darwin" ]; then
    # if [ $ret_code -ne 0 ]; then 
    #    exit $ret_code
    # fi
-   # compile_out=`$clang_pp_bin "$script_path/src/core/ebmcore/DataSetByAttribute.cpp" "$script_path/src/core/ebmcore/DataSetByAttributeCombination.cpp" "$script_path/src/core/ebmcore/InteractionDetection.cpp" "$script_path/src/core/ebmcore/Logging.cpp" "$script_path/src/core/ebmcore/SamplingWithReplacement.cpp" "$script_path/src/core/ebmcore/Training.cpp" -I"$script_path/src/core/ebmcore" -I"$script_path/src/core/inc" -std=c++11 -fpermissive -fvisibility=hidden -O3 -march=core2 -DEBMCORE_EXPORTS -fPIC -dynamiclib -m32 -install_name @rpath/ebmcore_mac_x86_debug.dylib -o "$script_path/tmp/clang/bin/debug/mac/x86/ebmcore/ebmcore_mac_x86_debug.dylib" 2>&1`
+   # compile_out=`$clang_pp_bin "$script_path/src/core/ebmcore/DataSetByAttribute.cpp" "$script_path/src/core/ebmcore/DataSetByAttributeCombination.cpp" "$script_path/src/core/ebmcore/InteractionDetection.cpp" "$script_path/src/core/ebmcore/Logging.cpp" "$script_path/src/core/ebmcore/SamplingWithReplacement.cpp" "$script_path/src/core/ebmcore/Training.cpp" -I"$script_path/src/core/ebmcore" -I"$script_path/src/core/inc" -std=c++11 -fpermissive -fvisibility=hidden -O3 -march=core2 -DEBMCORE_EXPORTS -fpic -dynamiclib -m32 -install_name @rpath/ebmcore_mac_x86_debug.dylib -o "$script_path/tmp/clang/bin/debug/mac/x86/ebmcore/ebmcore_mac_x86_debug.dylib" 2>&1`
    # ret_code=$?
    # echo -n "$compile_out"
    # echo -n "$compile_out" > "$script_path/tmp/clang/intermediate/debug/mac/x86/ebmcore/ebmcore_debug_mac_x86_build_log.txt"
@@ -162,7 +162,7 @@ elif [ "$os_type" = "Linux" ]; then
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   compile_out=`$g_pp_bin "$script_path/src/core/ebmcore/DataSetByAttribute.cpp" "$script_path/src/core/ebmcore/DataSetByAttributeCombination.cpp" "$script_path/src/core/ebmcore/InteractionDetection.cpp" "$script_path/src/core/ebmcore/Logging.cpp" "$script_path/src/core/ebmcore/SamplingWithReplacement.cpp" "$script_path/src/core/ebmcore/Training.cpp" -I"$script_path/src/core/ebmcore" -I"$script_path/src/core/inc" -std=c++11 -fpermissive -fvisibility=hidden -O3 -march=core2 -DEBMCORE_EXPORTS -fPIC -Wl,--wrap=memcpy "$script_path/src/core/ebmcore/WrapFunc.cpp" -static-libgcc -static-libstdc++ -shared -m64 -DNDEBUG -o "$script_path/tmp/gcc/bin/release/linux/x64/ebmcore/ebmcore_linux_x64.so" 2>&1`
+   compile_out=`$g_pp_bin "$script_path/src/core/ebmcore/DataSetByAttribute.cpp" "$script_path/src/core/ebmcore/DataSetByAttributeCombination.cpp" "$script_path/src/core/ebmcore/InteractionDetection.cpp" "$script_path/src/core/ebmcore/Logging.cpp" "$script_path/src/core/ebmcore/SamplingWithReplacement.cpp" "$script_path/src/core/ebmcore/Training.cpp" -I"$script_path/src/core/ebmcore" -I"$script_path/src/core/inc" -std=c++11 -fpermissive -fvisibility=hidden -O3 -march=core2 -DEBMCORE_EXPORTS -fpic -Wl,--wrap=memcpy "$script_path/src/core/ebmcore/WrapFunc.cpp" -static-libgcc -static-libstdc++ -shared -m64 -DNDEBUG -o "$script_path/tmp/gcc/bin/release/linux/x64/ebmcore/ebmcore_linux_x64.so" 2>&1`
    ret_code=$?
    echo -n "$compile_out"
    echo -n "$compile_out" > "$script_path/tmp/gcc/intermediate/release/linux/x64/ebmcore/ebmcore_release_linux_x64_build_log.txt"
@@ -191,7 +191,7 @@ elif [ "$os_type" = "Linux" ]; then
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   compile_out=`$g_pp_bin "$script_path/src/core/ebmcore/DataSetByAttribute.cpp" "$script_path/src/core/ebmcore/DataSetByAttributeCombination.cpp" "$script_path/src/core/ebmcore/InteractionDetection.cpp" "$script_path/src/core/ebmcore/Logging.cpp" "$script_path/src/core/ebmcore/SamplingWithReplacement.cpp" "$script_path/src/core/ebmcore/Training.cpp" -I"$script_path/src/core/ebmcore" -I"$script_path/src/core/inc" -std=c++11 -fpermissive -fvisibility=hidden -O3 -march=core2 -DEBMCORE_EXPORTS -fPIC -Wl,--wrap=memcpy "$script_path/src/core/ebmcore/WrapFunc.cpp" -static-libgcc -static-libstdc++ -shared -m64 -o "$script_path/tmp/gcc/bin/debug/linux/x64/ebmcore/ebmcore_linux_x64_debug.so" 2>&1`
+   compile_out=`$g_pp_bin "$script_path/src/core/ebmcore/DataSetByAttribute.cpp" "$script_path/src/core/ebmcore/DataSetByAttributeCombination.cpp" "$script_path/src/core/ebmcore/InteractionDetection.cpp" "$script_path/src/core/ebmcore/Logging.cpp" "$script_path/src/core/ebmcore/SamplingWithReplacement.cpp" "$script_path/src/core/ebmcore/Training.cpp" -I"$script_path/src/core/ebmcore" -I"$script_path/src/core/inc" -std=c++11 -fpermissive -fvisibility=hidden -O3 -march=core2 -DEBMCORE_EXPORTS -fpic -Wl,--wrap=memcpy "$script_path/src/core/ebmcore/WrapFunc.cpp" -static-libgcc -static-libstdc++ -shared -m64 -o "$script_path/tmp/gcc/bin/debug/linux/x64/ebmcore/ebmcore_linux_x64_debug.so" 2>&1`
    ret_code=$?
    echo -n "$compile_out"
    echo -n "$compile_out" > "$script_path/tmp/gcc/intermediate/debug/linux/x64/ebmcore/ebmcore_debug_linux_x64_build_log.txt"
@@ -220,7 +220,7 @@ elif [ "$os_type" = "Linux" ]; then
    # if [ $ret_code -ne 0 ]; then 
    #    exit $ret_code
    # fi
-   # compile_out=`$g_pp_bin "$script_path/src/core/ebmcore/DataSetByAttribute.cpp" "$script_path/src/core/ebmcore/DataSetByAttributeCombination.cpp" "$script_path/src/core/ebmcore/InteractionDetection.cpp" "$script_path/src/core/ebmcore/Logging.cpp" "$script_path/src/core/ebmcore/SamplingWithReplacement.cpp" "$script_path/src/core/ebmcore/Training.cpp" -I"$script_path/src/core/ebmcore" -I"$script_path/src/core/inc" -std=c++11 -fpermissive -fvisibility=hidden -O3 -march=core2 -DEBMCORE_EXPORTS -fPIC -Wl,--wrap=memcpy "$script_path/src/core/ebmcore/WrapFunc.cpp" -static-libgcc -static-libstdc++ -shared -m32 -DNDEBUG -o "$script_path/tmp/gcc/bin/release/linux/x86/ebmcore/ebmcore_linux_x86.so" 2>&1`
+   # compile_out=`$g_pp_bin "$script_path/src/core/ebmcore/DataSetByAttribute.cpp" "$script_path/src/core/ebmcore/DataSetByAttributeCombination.cpp" "$script_path/src/core/ebmcore/InteractionDetection.cpp" "$script_path/src/core/ebmcore/Logging.cpp" "$script_path/src/core/ebmcore/SamplingWithReplacement.cpp" "$script_path/src/core/ebmcore/Training.cpp" -I"$script_path/src/core/ebmcore" -I"$script_path/src/core/inc" -std=c++11 -fpermissive -fvisibility=hidden -O3 -march=core2 -DEBMCORE_EXPORTS -fpic -Wl,--wrap=memcpy "$script_path/src/core/ebmcore/WrapFunc.cpp" -static-libgcc -static-libstdc++ -shared -m32 -DNDEBUG -o "$script_path/tmp/gcc/bin/release/linux/x86/ebmcore/ebmcore_linux_x86.so" 2>&1`
    # ret_code=$?
    # echo -n "$compile_out"
    # echo -n "$compile_out" > "$script_path/tmp/gcc/intermediate/release/linux/x86/ebmcore/ebmcore_release_linux_x86_build_log.txt"
@@ -249,7 +249,7 @@ elif [ "$os_type" = "Linux" ]; then
    # if [ $ret_code -ne 0 ]; then 
    #    exit $ret_code
    # fi
-   # compile_out=`$g_pp_bin "$script_path/src/core/ebmcore/DataSetByAttribute.cpp" "$script_path/src/core/ebmcore/DataSetByAttributeCombination.cpp" "$script_path/src/core/ebmcore/InteractionDetection.cpp" "$script_path/src/core/ebmcore/Logging.cpp" "$script_path/src/core/ebmcore/SamplingWithReplacement.cpp" "$script_path/src/core/ebmcore/Training.cpp" -I"$script_path/src/core/ebmcore" -I"$script_path/src/core/inc" -std=c++11 -fpermissive -fvisibility=hidden -O3 -march=core2 -DEBMCORE_EXPORTS -fPIC -Wl,--wrap=memcpy "$script_path/src/core/ebmcore/WrapFunc.cpp" -static-libgcc -static-libstdc++ -shared -m32 -o "$script_path/tmp/gcc/bin/debug/linux/x86/ebmcore/ebmcore_linux_x86_debug.so" 2>&1`
+   # compile_out=`$g_pp_bin "$script_path/src/core/ebmcore/DataSetByAttribute.cpp" "$script_path/src/core/ebmcore/DataSetByAttributeCombination.cpp" "$script_path/src/core/ebmcore/InteractionDetection.cpp" "$script_path/src/core/ebmcore/Logging.cpp" "$script_path/src/core/ebmcore/SamplingWithReplacement.cpp" "$script_path/src/core/ebmcore/Training.cpp" -I"$script_path/src/core/ebmcore" -I"$script_path/src/core/inc" -std=c++11 -fpermissive -fvisibility=hidden -O3 -march=core2 -DEBMCORE_EXPORTS -fpic -Wl,--wrap=memcpy "$script_path/src/core/ebmcore/WrapFunc.cpp" -static-libgcc -static-libstdc++ -shared -m32 -o "$script_path/tmp/gcc/bin/debug/linux/x86/ebmcore/ebmcore_linux_x86_debug.so" 2>&1`
    # ret_code=$?
    # echo -n "$compile_out"
    # echo -n "$compile_out" > "$script_path/tmp/gcc/intermediate/debug/linux/x86/ebmcore/ebmcore_debug_linux_x86_build_log.txt"
