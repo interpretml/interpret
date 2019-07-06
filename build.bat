3725b4f8fa4770b86acc9a6aded7e60b2e584e80
Interpret ML <interpretml@outlook.com>
2019-07-06 16:48:09 -0700

    change build.bat to build the core project file instead of the solution

--- build.bat
+++ build.bat
@@ -3,22 +3,22 @@ SETLOCAL
 
 SET script_path=%~dp0
 
-MSBuild.exe "%script_path%src\core\emlcore.sln" /p:Configuration=Release /p:Platform=x64
+MSBuild.exe "%script_path%src\core\ebmcore\ebmcore.vcxproj" /p:Configuration=Release /p:Platform=x64
 IF %ERRORLEVEL% NEQ 0 (
    ECHO MSBuild for Release x64 returned error code %ERRORLEVEL%
    EXIT /B %ERRORLEVEL%
 )
-MSBuild.exe "%script_path%src\core\emlcore.sln" /p:Configuration=Release /p:Platform=x86
+MSBuild.exe "%script_path%src\core\ebmcore\ebmcore.vcxproj" /p:Configuration=Release /p:Platform=x86
 IF %ERRORLEVEL% NEQ 0 (
    ECHO MSBuild for Release x86 returned error code %ERRORLEVEL%
    EXIT /B %ERRORLEVEL%
 )
-MSBuild.exe "%script_path%src\core\emlcore.sln" /p:Configuration=Debug /p:Platform=x64
+MSBuild.exe "%script_path%src\core\ebmcore\ebmcore.vcxproj" /p:Configuration=Debug /p:Platform=x64
 IF %ERRORLEVEL% NEQ 0 (
    ECHO MSBuild for Debug x64 returned error code %ERRORLEVEL%
    EXIT /B %ERRORLEVEL%
 )
-MSBuild.exe "%script_path%src\core\emlcore.sln" /p:Configuration=Debug /p:Platform=x86
+MSBuild.exe "%script_path%src\core\ebmcore\ebmcore.vcxproj" /p:Configuration=Debug /p:Platform=x86
 IF %ERRORLEVEL% NEQ 0 (
    ECHO MSBuild for Debug x86 returned error code %ERRORLEVEL%
    EXIT /B %ERRORLEVEL%
