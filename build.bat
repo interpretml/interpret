884fd8ed8a24d89417912457854109e646c7fddd
Interpret ML <interpretml@outlook.com>
2019-07-06 17:09:28 -0700

    update platform in VS for consistency

--- build.bat
+++ build.bat
@@ -8,7 +8,7 @@ IF %ERRORLEVEL% NEQ 0 (
    ECHO MSBuild for Release x64 returned error code %ERRORLEVEL%
    EXIT /B %ERRORLEVEL%
 )
-MSBuild.exe "%script_path%src\core\ebmcore\ebmcore.vcxproj" /p:Configuration=Release /p:Platform=x86
+MSBuild.exe "%script_path%src\core\ebmcore\ebmcore.vcxproj" /p:Configuration=Release /p:Platform=Win32
 IF %ERRORLEVEL% NEQ 0 (
    ECHO MSBuild for Release x86 returned error code %ERRORLEVEL%
    EXIT /B %ERRORLEVEL%
@@ -18,7 +18,7 @@ IF %ERRORLEVEL% NEQ 0 (
    ECHO MSBuild for Debug x64 returned error code %ERRORLEVEL%
    EXIT /B %ERRORLEVEL%
 )
-MSBuild.exe "%script_path%src\core\ebmcore\ebmcore.vcxproj" /p:Configuration=Debug /p:Platform=x86
+MSBuild.exe "%script_path%src\core\ebmcore\ebmcore.vcxproj" /p:Configuration=Debug /p:Platform=Win32
 IF %ERRORLEVEL% NEQ 0 (
    ECHO MSBuild for Debug x86 returned error code %ERRORLEVEL%
    EXIT /B %ERRORLEVEL%
