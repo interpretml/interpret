74b1e133841063e311ecb873c48e74e8bd402fdc
Interpret ML <interpretml@outlook.com>
2019-07-12 00:33:13 -0700

    add building for other platforms in test_core_api.sh

--- src/core/TestCoreApi/test_core_api.sh
+++ src/core/TestCoreApi/test_core_api.sh
@@ -21,8 +21,9 @@ compile_all="\"$root_path/src/core/TestCoreApi/TestCoreApi.cpp\" -I\"$root_path/
 
 if [ "$os_type" = "Darwin" ]; then
    # reference on rpath & install_name: https://www.mikeash.com/pyblog/friday-qa-2009-11-06-linking-and-install-names.html
+   # the -l<library> parameter for some reason adds a lib at the start and .dylib at the end
 
-   compile_mac="$compile_all"
+   compile_mac="$compile_all -L\"$root_path/staging\" -Wl,-rpath,@loader_path"
 
    echo "Compiling TestCoreApi with $clang_pp_bin for macOS release|x64"
    [ -d "$root_path/tmp/clang/intermediate/release/mac/x64/TestCoreApi" ] || mkdir -p "$root_path/tmp/clang/intermediate/release/mac/x64/TestCoreApi"
@@ -35,15 +36,21 @@ if [ "$os_type" = "Darwin" ]; then
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
+   compile_command="$clang_pp_bin $compile_mac -m64 -DNDEBUG -l_ebmcore_mac_x64 -o \"$root_path/tmp/clang/bin/release/mac/x64/TestCoreApi/test_core_api\" 2>&1"
+   compile_out=`eval $compile_command`
+   ret_code=$?
+   echo -n "$compile_out"
+   echo -n "$compile_out" > "$root_path/tmp/clang/intermediate/release/mac/x64/TestCoreApi/TestCoreApi_release_mac_x64_build_log.txt"
+   if [ $ret_code -ne 0 ]; then 
+      exit $ret_code
+   fi
    cp "$root_path/staging/lib_ebmcore_mac_x64.dylib" "$root_path/tmp/clang/bin/release/mac/x64/TestCoreApi/"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   compile_out=`eval $clang_pp_bin $compile_mac -m64 -DNDEBUG -install_name @rpath/TestCoreApi_mac_x64.dylib -o \"$root_path/tmp/clang/bin/release/mac/x64/TestCoreApi/test_core_api\" 2>&1`
+   "$root_path/tmp/clang/bin/release/linux/x64/TestCoreApi/test_core_api"
    ret_code=$?
-   echo -n "$compile_out"
-   echo -n "$compile_out" > "$root_path/tmp/clang/intermediate/release/mac/x64/TestCoreApi/TestCoreApi_release_mac_x64_build_log.txt"
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
@@ -59,15 +66,21 @@ if [ "$os_type" = "Darwin" ]; then
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
+   compile_command="$clang_pp_bin $compile_mac -m64 -l_ebmcore_mac_x64_debug -o \"$root_path/tmp/clang/bin/debug/mac/x64/TestCoreApi/test_core_api\" 2>&1"
+   compile_out=`eval $compile_command`
+   ret_code=$?
+   echo -n "$compile_out"
+   echo -n "$compile_out" > "$root_path/tmp/clang/intermediate/debug/mac/x64/TestCoreApi/TestCoreApi_debug_mac_x64_build_log.txt"
+   if [ $ret_code -ne 0 ]; then 
+      exit $ret_code
+   fi
    cp "$root_path/staging/lib_ebmcore_mac_x64_debug.dylib" "$root_path/tmp/clang/bin/debug/mac/x64/TestCoreApi/"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   compile_out=`eval $clang_pp_bin $compile_mac -m64 -install_name @rpath/TestCoreApi_mac_x64_debug.dylib -o \"$root_path/tmp/clang/bin/debug/mac/x64/TestCoreApi/test_core_api\" 2>&1`
+   "$root_path/tmp/clang/bin/debug/linux/x64/TestCoreApi/test_core_api"
    ret_code=$?
-   echo -n "$compile_out"
-   echo -n "$compile_out" > "$root_path/tmp/clang/intermediate/debug/mac/x64/TestCoreApi/TestCoreApi_debug_mac_x64_build_log.txt"
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
@@ -83,15 +96,21 @@ if [ "$os_type" = "Darwin" ]; then
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
+   compile_command="$clang_pp_bin $compile_mac -m32 -DNDEBUG -l_ebmcore_mac_x86 -o \"$root_path/tmp/clang/bin/release/mac/x86/TestCoreApi/test_core_api\" 2>&1"
+   compile_out=`eval $compile_command`
+   ret_code=$?
+   echo -n "$compile_out"
+   echo -n "$compile_out" > "$root_path/tmp/clang/intermediate/release/mac/x86/TestCoreApi/TestCoreApi_release_mac_x86_build_log.txt"
+   if [ $ret_code -ne 0 ]; then 
+      exit $ret_code
+   fi
    cp "$root_path/staging/lib_ebmcore_mac_x86.dylib" "$root_path/tmp/clang/bin/release/mac/x86/TestCoreApi/"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   compile_out=`eval $clang_pp_bin $compile_mac -m32 -DNDEBUG -install_name @rpath/TestCoreApi_mac_x86.dylib -o \"$root_path/tmp/clang/bin/release/mac/x86/TestCoreApi/test_core_api\" 2>&1`
+   "$root_path/tmp/clang/bin/release/linux/x86/TestCoreApi/test_core_api"
    ret_code=$?
-   echo -n "$compile_out"
-   echo -n "$compile_out" > "$root_path/tmp/clang/intermediate/release/mac/x86/TestCoreApi/TestCoreApi_release_mac_x86_build_log.txt"
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
@@ -107,49 +126,31 @@ if [ "$os_type" = "Darwin" ]; then
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   cp "$root_path/staging/lib_ebmcore_mac_x86_debug.dylib" "$root_path/tmp/clang/bin/debug/mac/x86/TestCoreApi/"
-   ret_code=$?
-   if [ $ret_code -ne 0 ]; then 
-      exit $ret_code
-   fi
-   compile_out=`eval $clang_pp_bin $compile_mac -m32 -install_name @rpath/TestCoreApi_mac_x86_debug.dylib -o \"$root_path/tmp/clang/bin/debug/mac/x86/TestCoreApi/test_core_api\" 2>&1`
+   compile_command="$clang_pp_bin $compile_mac -m32 -l_ebmcore_mac_x86_debug -o \"$root_path/tmp/clang/bin/debug/mac/x86/TestCoreApi/test_core_api\" 2>&1"
+   compile_out=`eval $compile_command`
    ret_code=$?
    echo -n "$compile_out"
    echo -n "$compile_out" > "$root_path/tmp/clang/intermediate/debug/mac/x86/TestCoreApi/TestCoreApi_debug_mac_x86_build_log.txt"
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
+   cp "$root_path/staging/lib_ebmcore_mac_x86_debug.dylib" "$root_path/tmp/clang/bin/debug/mac/x86/TestCoreApi/"
+   ret_code=$?
+   if [ $ret_code -ne 0 ]; then 
+      exit $ret_code
+   fi
+   "$root_path/tmp/clang/bin/debug/linux/x86/TestCoreApi/test_core_api"
+   ret_code=$?
+   if [ $ret_code -ne 0 ]; then 
+      exit $ret_code
+   fi
+
 elif [ "$os_type" = "Linux" ]; then
    # to cross compile for different architectures x86/x64, run the following command: sudo apt-get install g++-multilib
    # "readelf -d <lib_filename.so>" should show library rpath:    $ORIGIN/    OR    ${ORIGIN}/    for Linux so that the console app will find the core library in the same directory as the app: https://stackoverflow.com/questions/6288206/lookup-failure-when-linking-using-rpath-and-origin
    # the -l<library> parameter for some reason adds a lib at the start and .so at the end
 
    compile_linux="$compile_all -L\"$root_path/staging\" -Wl,-rpath-link,\"$root_path/staging\" -Wl,-rpath,'\$ORIGIN/'"
-
-
-
-
-
-
-
-
-
-
-
-
-
-   if [ "$os_type" = "not_true" ]; then
-
-
-
-
-
-
-
-
-
-
-
 
    echo "Compiling TestCoreApi with $g_pp_bin for Linux release|x64"
    [ -d "$root_path/tmp/gcc/intermediate/release/linux/x64/TestCoreApi" ] || mkdir -p "$root_path/tmp/gcc/intermediate/release/linux/x64/TestCoreApi"
@@ -162,15 +163,21 @@ elif [ "$os_type" = "Linux" ]; then
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
+   compile_command="$g_pp_bin $compile_linux -m64 -DNDEBUG -l_ebmcore_linux_x64 -o \"$root_path/tmp/gcc/bin/release/linux/x64/TestCoreApi/test_core_api\" 2>&1"
+   compile_out=`eval $compile_command`
+   ret_code=$?
+   echo -n "$compile_out"
+   echo -n "$compile_out" > "$root_path/tmp/gcc/intermediate/release/linux/x64/TestCoreApi/TestCoreApi_release_linux_x64_build_log.txt"
+   if [ $ret_code -ne 0 ]; then 
+      exit $ret_code
+   fi
    cp "$root_path/staging/lib_ebmcore_linux_x64.so" "$root_path/tmp/gcc/bin/release/linux/x64/TestCoreApi/"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   compile_out=`eval $g_pp_bin $compile_linux -m64 -DNDEBUG -o \"$root_path/tmp/gcc/bin/release/linux/x64/TestCoreApi/test_core_api\" 2>&1`
+   "$root_path/tmp/gcc/bin/release/linux/x64/TestCoreApi/test_core_api"
    ret_code=$?
-   echo -n "$compile_out"
-   echo -n "$compile_out" > "$root_path/tmp/gcc/intermediate/release/linux/x64/TestCoreApi/TestCoreApi_release_linux_x64_build_log.txt"
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
@@ -186,15 +193,21 @@ elif [ "$os_type" = "Linux" ]; then
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
+   compile_command="$g_pp_bin $compile_linux -m64 -l_ebmcore_linux_x64_debug -o \"$root_path/tmp/gcc/bin/debug/linux/x64/TestCoreApi/test_core_api\" 2>&1"
+   compile_out=`eval $compile_command`
+   ret_code=$?
+   echo -n "$compile_out"
+   echo -n "$compile_out" > "$root_path/tmp/gcc/intermediate/debug/linux/x64/TestCoreApi/TestCoreApi_debug_linux_x64_build_log.txt"
+   if [ $ret_code -ne 0 ]; then 
+      exit $ret_code
+   fi
    cp "$root_path/staging/lib_ebmcore_linux_x64_debug.so" "$root_path/tmp/gcc/bin/debug/linux/x64/TestCoreApi/"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   compile_out=`eval $g_pp_bin $compile_linux -m64 -o \"$root_path/tmp/gcc/bin/debug/linux/x64/TestCoreApi/test_core_api\" 2>&1`
+   "$root_path/tmp/gcc/bin/debug/linux/x64/TestCoreApi/test_core_api"
    ret_code=$?
-   echo -n "$compile_out"
-   echo -n "$compile_out" > "$root_path/tmp/gcc/intermediate/debug/linux/x64/TestCoreApi/TestCoreApi_debug_linux_x64_build_log.txt"
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
@@ -210,42 +223,24 @@ elif [ "$os_type" = "Linux" ]; then
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   cp "$root_path/staging/lib_ebmcore_linux_x86.so" "$root_path/tmp/gcc/bin/release/linux/x86/TestCoreApi/"
-   ret_code=$?
-   if [ $ret_code -ne 0 ]; then 
-      exit $ret_code
-   fi
-   compile_out=`eval $g_pp_bin $compile_linux -m32 -DNDEBUG -o \"$root_path/tmp/gcc/bin/release/linux/x86/TestCoreApi/test_core_api\" 2>&1`
+   compile_command="$g_pp_bin $compile_linux -m32 -DNDEBUG -l_ebmcore_linux_x86 -o \"$root_path/tmp/gcc/bin/release/linux/x86/TestCoreApi/test_core_api\" 2>&1"
+   compile_out=`eval $compile_command`
    ret_code=$?
    echo -n "$compile_out"
    echo -n "$compile_out" > "$root_path/tmp/gcc/intermediate/release/linux/x86/TestCoreApi/TestCoreApi_release_linux_x86_build_log.txt"
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-
-
-
-
-
-
-
-
-
-
-
-
-
+   cp "$root_path/staging/lib_ebmcore_linux_x86.so" "$root_path/tmp/gcc/bin/release/linux/x86/TestCoreApi/"
+   ret_code=$?
+   if [ $ret_code -ne 0 ]; then 
+      exit $ret_code
    fi
-
-
-
-
-
-
-
-
-
-
+   "$root_path/tmp/gcc/bin/release/linux/x86/TestCoreApi/test_core_api"
+   ret_code=$?
+   if [ $ret_code -ne 0 ]; then 
+      exit $ret_code
+   fi
 
    echo "Compiling TestCoreApi with $g_pp_bin for Linux debug|x86"
    [ -d "$root_path/tmp/gcc/intermediate/debug/linux/x86/TestCoreApi" ] || mkdir -p "$root_path/tmp/gcc/intermediate/debug/linux/x86/TestCoreApi"
