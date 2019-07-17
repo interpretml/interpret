5f6a0d2d37edcb469a72c7a50d2d50de7fd15cb6
Eduardo de Leon <eddeleon@microsoft.com>
2019-07-17 17:40:48 -0400

    Add registration of blackbox explainers with an example

---
+++ src/python/interpret/ext/blackbox/__init__.py
@@ -0,0 +1,19 @@
+import logging
+import sys
+
+from interpret.ext.extension_utils import load_class_extensions
+
+module_logger = logging.getLogger(__name__)
+
+BLACKBOX_EXTENSION_KEY = "interpret_ext_blackbox"
+
+
+def _is_valid_blackbox_explainer(proposed_blackbox_explainer):
+    return True
+
+
+# How to get the current module
+# https://stackoverflow.com/questions/1676835
+current_module = sys.modules[__name__]
+
+load_class_extensions(current_module, BLACKBOX_EXTENSION_KEY, _is_valid_blackbox_explainer)
