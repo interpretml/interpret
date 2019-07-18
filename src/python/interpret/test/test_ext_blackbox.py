0ece70b0e38eeec60ad6201c62f9b30dde090eff
Eduardo de Leon <eddeleon@microsoft.com>
2019-07-18 13:09:33 -0400

    Update test to give a better error message

--- src/python/interpret/test/test_ext_blackbox.py
+++ src/python/interpret/test/test_ext_blackbox.py
@@ -1,7 +1,9 @@
-import interpret.ext.blackbox
+from six import raise_from
 
 
 def test_import_demo_explainer():
-    print(dir(interpret.ext.blackbox))
-    from interpret.ext.blackbox import BlackboxExplainerExample
-    print("Loaded {}".format(BlackboxExplainerExample.__name__))
+    try:
+        from interpret.ext.blackbox import BlackboxExplainerExample  # noqa
+    except ImportError as import_error:
+        raise_from(Exception("Failure in interpret.ext.blackbox while trying "
+                             "to load example explainers through extension_utils", import_error))
