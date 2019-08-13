acbcb1eed4c47f0357c3cdb79c571c80c278e431
Interpret ML <interpretml@outlook.com>
2019-08-12 21:36:37 -0700

    Hardened blackbox ext validation. Fleshed out respective example.

--- python/interpret/ext/blackbox/__init__.py
+++ python/interpret/ext/blackbox/__init__.py
@@ -11,11 +11,24 @@ module_logger = logging.getLogger(__name__)
 BLACKBOX_EXTENSION_KEY = "interpret_ext_blackbox"
 
 
+# TODO: More checks for blackbox validation, specifically on spec for explainer/explanation when instantiated.
 def _is_valid_blackbox_explainer(proposed_blackbox_explainer):
-    for explanation_type in ["local", "global", "perf", "data"]:
-        if hasattr(proposed_blackbox_explainer, "explain_" + explanation_type):
-            return True
-    return False
+    explainer_type = proposed_blackbox_explainer.explainer_type
+    available_explanations = proposed_blackbox_explainer.available_explanations
+
+    if explainer_type != "blackbox":
+        module_logger.warning("Proposed explainer is not a blackbox.")
+        return False
+
+    for available_explanation in available_explanations:
+        has_explain_method = hasattr(proposed_blackbox_explainer, "explain_" + available_explanation)
+        if not has_explain_method:
+            module_logger.warning(
+                "Proposed explainer has available explanation {} but has no respective method.".format(available_explanation)
+            )
+            return False
+
+    return True
 
 
 # How to get the current module
--- python/interpret/test/test_ext_blackbox.py
+++ python/interpret/test/test_ext_blackbox.py
@@ -3,6 +3,11 @@
 
 from six import raise_from
 
+from .utils import assert_valid_explanation
+from .utils import synthetic_classification
+
+from ..glassbox import LogisticRegression
+
 
 def test_import_demo_explainer():
     try:
@@ -13,5 +18,19 @@ def test_import_demo_explainer():
                 "Failure in interpret.ext.blackbox while trying "
                 "to load example explainers through extension_utils",
                 import_error,
-            )
+            ),
+            None,
         )
+
+
+def test_demo_explainer():
+    from interpret.ext.blackbox import BlackboxExplainerExample
+
+    data = synthetic_classification()
+    blackbox = LogisticRegression()
+    blackbox.fit(data["train"]["X"], data["train"]["y"])
+    predict_fn = lambda x: blackbox.predict_proba(x)  # noqa: E731
+
+    explainer = BlackboxExplainerExample(predict_fn, data["train"]["X"])
+    explanation = explainer.explain_local(data["test"]["X"].head(), data["test"]["y"].head())
+    assert_valid_explanation(explanation)
