27e77e0e633fa63db06213ce92ce496f78d5477a
Interpret ML <interpretml@outlook.com>
2019-09-22 17:58:50 -0700

    Hardened build-js-bundles to work both locally and on Azure DevOps.

--- python/scripts/build-js-bundles.py
+++ python/scripts/build-js-bundles.py
@@ -4,17 +4,27 @@
 
 import subprocess
 import os
+import sys
 from shutil import copyfile
 
 
 if __name__ == '__main__':
+    in_devops = False
+    if len(sys.argv) == 2 and sys.argv[1] == "devops":
+        in_devops = True
+
     script_path = os.path.dirname(os.path.abspath(__file__))
     js_dir = os.path.join(script_path, "..", "interpret-core", "js")
 
     # NOTE: Using shell=True can be a security hazard where there is user inputs.
     # In this case, there are no user inputs.
-    subprocess.run(["npm install"], cwd=js_dir, shell=True)
-    subprocess.run(["npm run build-prod"], cwd=js_dir, shell=True)
+    # NOTE: Workaround for Azure DevOps.
+    if in_devops:
+        subprocess.run(["npm install"], cwd=js_dir, shell=True)
+        subprocess.run(["npm run build-prod"], cwd=js_dir, shell=True)
+    else:
+        subprocess.run(["npm", "install"], cwd=js_dir, shell=True)
+        subprocess.run(["npm", "run", "build-prod"], cwd=js_dir, shell=True)
     js_bundle_src = os.path.join(js_dir, "dist", "bundle.js")
     js_bundle_dest = os.path.join(
         script_path, "..", "interpret-core",
