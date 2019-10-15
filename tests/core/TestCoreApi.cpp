e09a542561951a06f40d34d317b24419804418bc
Interpret ML <interpretml@outlook.com>
2019-10-14 17:29:25 -0700

    add C++ tests for data bin packing in classification

--- tests/core/TestCoreApi.cpp
+++ tests/core/TestCoreApi.cpp
@@ -2173,7 +2173,7 @@ TEST_CASE("Test Rehydration, training, multiclass") {
    }
 }
 
-TEST_CASE("Test data bit packing extremes, training") {
+TEST_CASE("Test data bit packing extremes, training, regression") {
    for(size_t exponentialBins = 1; exponentialBins < 10; ++exponentialBins) {
       IntegerDataType exponential = static_cast<IntegerDataType>(std::pow(2, exponentialBins));
       // if we set the number of bins to be exponential, then we'll be just under a bit packing boundary.  4 bins means bits packs 00, 01, 10, and 11
@@ -2204,7 +2204,38 @@ TEST_CASE("Test data bit packing extremes, training") {
    }
 }
 
-TEST_CASE("Test data bit packing extremes, interaction") {
+TEST_CASE("Test data bit packing extremes, training, binary") {
+   for(size_t exponentialBins = 1; exponentialBins < 10; ++exponentialBins) {
+      IntegerDataType exponential = static_cast<IntegerDataType>(std::pow(2, exponentialBins));
+      // if we set the number of bins to be exponential, then we'll be just under a bit packing boundary.  4 bins means bits packs 00, 01, 10, and 11
+      for(IntegerDataType iRange = IntegerDataType { -1 }; iRange <= IntegerDataType { 1 }; ++iRange) {
+         IntegerDataType cBins = exponential + iRange; // check one less than the tight fit, the tight fit, and one above the tight fit
+         // try everything from 0 instances to 65 instances because for bitpacks with 1 bit, we can have up to 64 packed into a single data value on a 64 bit machine
+         for(size_t cInstances = 1; cInstances < 66; ++cInstances) {
+            TestApi test = TestApi(2);
+            test.AddFeatures({ FeatureTest(cBins) });
+            test.AddFeatureCombinations({ { 0 } });
+
+            std::vector<ClassificationInstance> trainingInstances;
+            std::vector<ClassificationInstance> validationInstances;
+            for(size_t iInstance = 0; iInstance < cInstances; ++iInstance) {
+               trainingInstances.push_back(ClassificationInstance(0, { cBins - 1 }));
+               validationInstances.push_back(ClassificationInstance(1, { cBins - 1 }));
+            }
+            test.AddTrainingInstances(trainingInstances);
+            test.AddValidationInstances(validationInstances);
+            test.InitializeTraining();
+
+            FractionalDataType validationMetric = test.Train(0);
+            CHECK_APPROX(validationMetric, cInstances * 0.70319717972663420);
+            FractionalDataType modelValue = test.GetCurrentModelPredictorScore(0, { static_cast<size_t>(cBins - 1) }, 1);
+            CHECK_APPROX(modelValue, -0.02);
+         }
+      }
+   }
+}
+
+TEST_CASE("Test data bit packing extremes, interaction, regression") {
    for(size_t exponentialBins = 1; exponentialBins < 10; ++exponentialBins) {
       IntegerDataType exponential = static_cast<IntegerDataType>(std::pow(2, exponentialBins));
       // if we set the number of bins to be exponential, then we'll be just under a bit packing boundary.  4 bins means bits packs 00, 01, 10, and 11
@@ -2232,6 +2263,36 @@ TEST_CASE("Test data bit packing extremes, interaction") {
       }
    }
 }
+
+TEST_CASE("Test data bit packing extremes, interaction, binary") {
+   for(size_t exponentialBins = 1; exponentialBins < 10; ++exponentialBins) {
+      IntegerDataType exponential = static_cast<IntegerDataType>(std::pow(2, exponentialBins));
+      // if we set the number of bins to be exponential, then we'll be just under a bit packing boundary.  4 bins means bits packs 00, 01, 10, and 11
+      for(IntegerDataType iRange = IntegerDataType { -1 }; iRange <= IntegerDataType { 1 }; ++iRange) {
+         IntegerDataType cBins = exponential + iRange; // check one less than the tight fit, the tight fit, and one above the tight fit
+         // try everything from 0 instances to 65 instances because for bitpacks with 1 bit, we can have up to 64 packed into a single data value on a 64 bit machine
+         for(size_t cInstances = 1; cInstances < 66; ++cInstances) {
+            TestApi test = TestApi(2);
+            test.AddFeatures({ FeatureTest(2), FeatureTest(cBins) });
+
+            std::vector<ClassificationInstance> instances;
+            for(size_t iInstance = 0; iInstance < cInstances; ++iInstance) {
+               instances.push_back(ClassificationInstance(1, { 0, cBins - 1 }));
+            }
+            test.AddInteractionInstances(instances);
+            test.InitializeInteraction();
+
+            FractionalDataType metric = test.InteractionScore({ 0, 1 });
+            if(cBins == 1) {
+               CHECK_APPROX(metric, 0);
+            } else {
+               CHECK_APPROX(metric, 0.25 * cInstances);
+            }
+         }
+      }
+   }
+}
+
 
 void EBMCORE_CALLING_CONVENTION LogMessage(signed char traceLevel, const char * message) {
    UNUSED(traceLevel);
