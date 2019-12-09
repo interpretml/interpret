a4085cad72815344cae5b38a6fc2d1a1094a4850
Interpret ML <interpretml@outlook.com>
2019-12-09 03:33:30 -0800

    add simplified ebm_classify to R

--- R/R/interpret_core.R
+++ R/R/interpret_core.R
@@ -40,4 +40,49 @@ create_main_feature_combinations <- function(features) {
    return(feature_combinations)
 }
 
+ebm_classify <- function(X, y, n_estimators = 16, test_size = 0.15, data_n_episodes = 2000, early_stopping_run_length = 50, learning_rate = 0.01, max_tree_splits = 2, min_cases_for_splits = 2, random_state = 42) {
+   col_names = colnames(X)
 
+   bin_edges <- vector(mode = "list", ncol(X))
+   # TODO: I know this binning is buggy.  Review
+   for(col_name in col_names) bin_edges[[col_name]] <- unique(quantile(X[[col_name]], seq(0,1, 1.0 / 256)))
+   for(col_name in col_names) bin_edges[[col_name]] <- bin_edges[[col_name]][2:(length(bin_edges[[col_name]])-1)]
+   for(col_name in col_names) X[[col_name]] <- as.integer(findInterval(X[[col_name]], bin_edges[[col_name]]))
+   features <- lapply(col_names, function(col_name) { ebm_feature(n_bins = length(bin_edges[[col_name]])) })
+   feature_combinations <- create_main_feature_combinations(features)
+   
+   set.seed(random_state)
+   val_indexes = sample(1:length(y), ceiling(length(y) * test_size))
+
+   X_train = X[-val_indexes,]
+   y_train = y[-val_indexes]
+   X_val = X[val_indexes,]
+   y_val = y[val_indexes] 
+
+   X_train_vec <- vector(mode = "numeric") # , ncol(X_train) * nrow(X_train)
+   for(col_name in col_names) X_train_vec[(length(X_train_vec) + 1):(length(X_train_vec) + length(X_train[[col_name]]))] <- X_train[[col_name]]
+
+   X_val_vec <- vector(mode = "numeric") # , ncol(X_val) * nrow(X_val)
+   for(col_name in col_names) X_val_vec[(length(X_val_vec) + 1):(length(X_val_vec) + length(X_val[[col_name]]))] <- X_val[[col_name]]
+
+   result_list = cyclic_gradient_boost(
+      "classification",
+      2,
+      features,
+      feature_combinations,
+      X_train_vec,
+      y_train,
+      NULL,
+      X_val_vec,
+      y_val,
+      NULL,
+      0,
+      random_state,
+      learning_rate,
+      max_tree_splits, 
+      min_cases_for_splits, 
+      data_n_episodes,
+      early_stopping_run_length
+   )
+   return(result_list)
+}
