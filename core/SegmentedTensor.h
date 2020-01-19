7bf7cca9df234c5341b543f00de832c908be88f4
Interpret ML <interpretml@outlook.com>
2020-01-19 12:01:40 -0800

    add comments about the future direction of the SegmentedTensor class for multi-machine and CPU usage.

--- core/SegmentedTensor.h
+++ core/SegmentedTensor.h
@@ -13,6 +13,77 @@
 #include "EbmInternal.h" // EBM_INLINE
 #include "Logging.h" // EBM_ASSERT & LOG
 
+// TODO: we need to radically change this data structure so that we can efficiently pass it between machines in a cluster AND within/between a GPU/CPU
+// This stucture should be:
+//   EXTERIOR_STRUCTURE -> our external pointers point to here
+//     offset_to_start_of_real_data (which is the offset_to_MAIN_DATA_STRUCTURE_in_big_endian_format)
+//     size_of_real_data (from offset_to_MAIN_DATA_STRUCTURE_in_big_endian_format to the last valid value)
+//   EMPTY_SPACE_FOR_DIMENSIONS_TO_GROW_INTO
+//   offset_to_MAIN_DATA_STRUCTURE_in_big_endian_format -> our external caller can reduce our memory size to this point optinally to pass between memory boundaries
+//   DIMENSION_0_COUNT (this is the current # of cuts, not the maximum, which we store elsewhere and pass into our class for processing)
+//   DIMENSION_0_CUT_POINTS
+//   DIMENSION_1_COUNT (this is the current # of cuts, not the maximum, which we store elsewhere and pass into our class for processing)
+//   DIMENSION_1_CUT_POINTS
+//   MAIN_DATA_STRUCTURE -> our internal pointsers point to here
+//     offset_to_DIMENSION_0_COUNT_or_zero_if_value_array_expanded_and_zero_offset_to_MAIN_DATA_STRUCTURE (we can find all the dimensions from this one offset)
+//     count_dimensions
+//     offset_to_external_structure
+//     is_little_endian_bool_in_big_endian_format (little endian vs big endian)
+//     all_other_data_that_does_not_resize
+//   VALUES_ARRAY_FLAT_EXPANDABLE (we've preallocated enough room for this, but we don't always need it)
+//   EMPTY_SPACE_FOR_VALUES_TO_GROW_INTO
+// Reasons:
+//   - our super-parallel algrithm needs to split up the data and have separate processing cores process their data
+//     their outputs will be partial histograms.  After a single cores does the tree buiding, that core will need to push the model updates to all the children nodes
+//   - in an MPI environment, or a Spark cluster, or in a GPU, we'll need to pass this data structure between nodes, so it will need to be memcopy-able,
+//     which means no pointers (use 64-bit offsets), and it means that the data needs to be in a single contiguous byte array
+//   - we might want our external caller to allocate this memory, because perhaps we might want the MPI communication layer or other network protocol to sit outside of C++,
+//     so we want to allocate the memory just once and we need to be able to determine the memory size before allocating it.
+//     we know ahead of time how many dimensions AND the maximum split points in all dimensions, so it's possible to pre-determine this
+//   - we use an easy/compatible external structure at the top that our caller can read without knowing our deep internals, but it provides enough information for
+//     our external caller to identify the core inner data that they can memcopy to annother memory address space
+//   - so, our external caller has a non-changing memory location that indicates the internal offset where they can start copying data from and a number of bytes
+//     to copy when they want to pass this memory to annother memory boundary.  This essentially strips the useless non-used space away for compression
+//   - if the caller needs to be allocated on some kind of top boundary, they can optionally start from the top and just add the 
+//     offset_to_start_of_real_data and size_of_real_data together to get the size they need to copy from the top.  This will include a little
+//     empty data at the top, but the empty dimension data is usually much less than the value data for interaction models, and the dimension data should almost
+//     always be pretty small, so this isn't a problem.
+//   - once we recieve the interor data structure we can read offset_to_MAIN_DATA_STRUCTURE_in_big_endian_format, which will be in a machine
+//     independent endian format (big endian), and we can then find our MAIN_DATA_STRUCTURE from there, and from MAIN_DATA_STRUCTURE we can reconstruct
+//     our original memory size with empty space if we want, optionally
+//   - the caller can pass us a pointer to the exterior data, and we can then find the offset_to_MAIN_DATA_STRUCTURE_in_big_endian_format from the header
+//     then we can find our MAIN_DATA_STRUCTURE from there
+//   - when we expand our tensor, we need to keep the dimension data constant and then we expand the values efficiently 
+//     (starting by moving the last value to where it's eventually going to go).  After we've expanded the values, we can expand the dimensions upwards
+//     by first moving the 0th dimension where it will ultimately end up, then processing each dimension in order
+//   - if we have dimension 0 at the top, and cut points heading downwards, we'll be reading the data forwards as we process it, which efficiently loads it into the cache
+//   - we don't need to update offset_to_MAIN_DATA_STRUCTURE_in_big_endian_format or the values in the EXTERIOR_STRUCTURE until we return from C++, so let it get out of date while we process things internally
+//   - if the tensor is purely for internal consumption, we don't even need to allocate the EXTERIOR_STRUCTURE and offset_to_MAIN_DATA_STRUCTURE_in_big_endian_format, 
+//     so our allocator should have a flag to indicate if these are required or not
+//   - the maximum number of cuts is part of the feature_combination definition, so we don't need to store that and pass that reduntant information arround
+//     We do store the current number of cuts because that changes.  This data structure should therefore have a dependency on the feature_combination definition
+//     since we'll need to read the maximum number of cuts.  The pointer to the feature_combination class can be passed in via the stack to any function that needs that information
+//   - use 64 bit values for all offsets, since nobody will ever need more than 64 bits (you need a non-trivial amount of mass even if you store one bit per atom) and
+//     we might pass these between 64 and 32 bit processes, but a few 64 bit offsets won't be a problem even for a 32-bit process.
+//   - EXPANDING:
+//     - eventually all our tensors will be expanded when doing the model update, because we want to use array lookups instead of binary search when applying the model 
+//       (array lookup is a huge speed boost over binary search)
+//     - our current and best models start out expanded since we want an easy way to communicate with our caller, and we eventualy expect almost all the cuts to be used anyways
+//     - we might as well also flip to epanded mode whenever all dimensions are fully expanded, even when we think we have a compressed model.  
+//       For things like bools or low numbers of cuts this could be frequent, and the non-compressed model is actually smaller when all dimensions have been expanded
+//     - once we've been expanded, we no longer need the cut points since the cut points are just incrementing integers.  
+//       The maximum number of cuts is stored outside of the data structure already because that's common between all machines/GPUs,
+//       so we don't need to pass that redundant information arround, so offset_to_start_of_real_data can point directly to
+//       MAIN_DATA_STRUCTURE directly (which is most optimial for compression)
+//     - IMPORTANT: if we've been expanded AND offset_to_start_of_real_data points directly to MAIN_DATA_STRUCTURE, then if the external caller
+//       reduces the data to just the interior used bytes, we still need a way to get to the MAIN_DATA_STRUCTURE.  If we use 
+//       offset_to_DIMENSION_0_COUNT_or_zero_if_value_array_expanded_and_zero_offset_to_MAIN_DATA_STRUCTURE to indicate expansion with a zero value then
+//       we can also use it for the tripple use as the offset_to_MAIN_DATA_STRUCTURE_in_big_endian_format value.  Our caller will
+//       see a zero in what it expects is the offset_to_MAIN_DATA_STRUCTURE_in_big_endian_format, and then it can find MAIN_DATA_STRUCTURE the same
+//       way it would normally by adding the zero to the address it has.  The nice thing is that zero is both big_endian and also little_endian, so it works for both.
+//     - the top item in our MAIN_DATA_STRUCTURE MUST be offset_to_DIMENSION_0_COUNT_or_zero_if_value_array_expanded_and_zero_offset_to_MAIN_DATA_STRUCTURE for
+//       our efficient tripple use above!
+
 // TODO : simplify this in our code by removing the templating.  We always use ActiveDataType and FractionalDataType, so we don't need something generic which just complicates reading the code later for no benefit to this project, and we want to make this class passable by copy, which means it'll have to be C compatible
 template<typename TDivisions, typename TValues>
 struct SegmentedTensor final {
