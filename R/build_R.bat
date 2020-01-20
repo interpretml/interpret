c1fdc5789d32812099a6c94c8f164db5de8faefb
Interpret ML <interpretml@outlook.com>
2020-01-19 20:26:09 -0800

    first pass of repo reorganize core directory into shared/native

--- R/build_R.bat
+++ R/build_R.bat
@@ -3,7 +3,7 @@ SETLOCAL
 
 SET root_path=%~dp0
 
-robocopy "%root_path%\" "%root_path%..\tmp\R" /S /PURGE /R:2 /NP /XF "%root_path%src\Makevars" /XF "%root_path%src\Makevars.interpret" /XF "%root_path%build_R.bat" /XF cran_lic.txt /XF LICENSE /XD "%root_path%src\core"
+robocopy "%root_path%\" "%root_path%..\tmp\R" /S /PURGE /R:2 /NP /XF "%root_path%src\Makevars" /XF "%root_path%src\Makevars.interpret" /XF "%root_path%build_R.bat" /XF cran_lic.txt /XF LICENSE /XD "%root_path%src\native"
 ECHO robocopy returned error code %ERRORLEVEL%
 IF %ERRORLEVEL% GEQ 8 (
    EXIT /B %ERRORLEVEL%
@@ -13,7 +13,7 @@ copy /Y "%root_path%src\Makevars.interpret" "%root_path%..\tmp\R\src\Makevars"
 
 copy /Y "%root_path%cran_lic.txt" "%root_path%..\tmp\R\LICENSE"
 
-robocopy "%root_path%..\core" "%root_path%..\tmp\R\src\core" /S /PURGE /R:2 /NP /XF wrap_func.cpp /XF DllMainCore.cpp /XF ebmcore.def /XF ebmcore.vcxproj /XF ebmcore.vcxproj.user /XF ebmcore_exports.txt /XF PrecompiledHeader.cpp
+robocopy "%root_path%..\shared\native" "%root_path%..\tmp\R\src\native" /S /PURGE /R:2 /NP /XF wrap_func.cpp /XF DllMainCore.cpp /XF ebmcore.def /XF ebmcore.vcxproj /XF ebmcore.vcxproj.user /XF ebmcore_exports.txt /XF PrecompiledHeader.cpp
 ECHO robocopy returned error code %ERRORLEVEL%
 IF %ERRORLEVEL% GEQ 8 (
    EXIT /B %ERRORLEVEL%
