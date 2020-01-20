e0c69d465bb8a70cf881bd70c4abc54bf704e519
Interpret ML <interpretml@outlook.com>
2020-01-20 04:15:09 -0800

    small changes to R building script to conform to ebm_native

--- R/build_R.bat
+++ R/build_R.bat
@@ -3,7 +3,7 @@ SETLOCAL
 
 SET root_path=%~dp0
 
-robocopy "%root_path%\" "%root_path%..\tmp\R" /S /PURGE /R:2 /NP /XF "%root_path%src\Makevars" /XF "%root_path%src\Makevars.interpret" /XF "%root_path%build_R.bat" /XF cran_lic.txt /XF LICENSE /XD "%root_path%src\native"
+robocopy "%root_path%\" "%root_path%..\tmp\R" /S /PURGE /R:2 /NP /XF "%root_path%src\Makevars" /XF "%root_path%src\Makevars.interpret" /XF "%root_path%build_R.bat" /XF cran_lic.txt /XF LICENSE /XD "%root_path%src\ebm_native"
 ECHO robocopy returned error code %ERRORLEVEL%
 IF %ERRORLEVEL% GEQ 8 (
    EXIT /B %ERRORLEVEL%
@@ -13,7 +13,7 @@ copy /Y "%root_path%src\Makevars.interpret" "%root_path%..\tmp\R\src\Makevars"
 
 copy /Y "%root_path%cran_lic.txt" "%root_path%..\tmp\R\LICENSE"
 
-robocopy "%root_path%..\shared\ebm_native" "%root_path%..\tmp\R\src\ebm_native" /S /PURGE /R:2 /NP /XF wrap_func.cpp /XF DllMainNative.cpp /XF ebm_native_exports.def /XF ebm_native.vcxproj /XF ebm_native.vcxproj.user /XF ebm_native_exports.txt /XF PrecompiledHeader.cpp
+robocopy "%root_path%..\shared\ebm_native" "%root_path%..\tmp\R\src\ebm_native" /S /PURGE /R:2 /NP /XF wrap_func.cpp /XF DllMainEbmNative.cpp /XF ebm_native_exports.def /XF ebm_native_exports.txt /XF ebm_native.vcxproj /XF ebm_native.vcxproj.user /XF PrecompiledHeader.cpp
 ECHO robocopy returned error code %ERRORLEVEL%
 IF %ERRORLEVEL% GEQ 8 (
    EXIT /B %ERRORLEVEL%
