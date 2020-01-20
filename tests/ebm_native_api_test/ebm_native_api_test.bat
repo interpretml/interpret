9adc221d13c9efa42dac24a6444ef625dac31074
Interpret ML <interpretml@outlook.com>
2020-01-20 03:10:29 -0800

    fixes for ebm_native_api_test build

--- tests/ebm_native_api_test/ebm_native_api_test.bat
+++ tests/ebm_native_api_test/ebm_native_api_test.bat
@@ -11,51 +11,51 @@ for %%x in (%*) do (
 )
 
 IF %build_core% EQU 1 (
-   ECHO Building Core library...
+   ECHO Building ebm_native library...
    CALL "%root_path%build.bat" -32bit
 ) ELSE (
-   ECHO Core library NOT being built
+   ECHO ebm_native library NOT being built
 )
 
-MSBuild.exe "%root_path%tests\native\TestCoreApi.vcxproj" /p:Configuration=Release /p:Platform=x64
+MSBuild.exe "%root_path%tests\ebm_native_api_test\ebm_native_api_test.vcxproj" /p:Configuration=Release /p:Platform=x64
 IF %ERRORLEVEL% NEQ 0 (
    ECHO MSBuild for Release x64 returned error code %ERRORLEVEL%
    EXIT /B %ERRORLEVEL%
 )
-MSBuild.exe "%root_path%tests\native\TestCoreApi.vcxproj" /p:Configuration=Release /p:Platform=Win32
+MSBuild.exe "%root_path%tests\ebm_native_api_test\ebm_native_api_test.vcxproj" /p:Configuration=Release /p:Platform=Win32
 IF %ERRORLEVEL% NEQ 0 (
    ECHO MSBuild for Release x86 returned error code %ERRORLEVEL%
    EXIT /B %ERRORLEVEL%
 )
-MSBuild.exe "%root_path%tests\native\TestCoreApi.vcxproj" /p:Configuration=Debug /p:Platform=x64
+MSBuild.exe "%root_path%tests\ebm_native_api_test\ebm_native_api_test.vcxproj" /p:Configuration=Debug /p:Platform=x64
 IF %ERRORLEVEL% NEQ 0 (
    ECHO MSBuild for Debug x64 returned error code %ERRORLEVEL%
    EXIT /B %ERRORLEVEL%
 )
-MSBuild.exe "%root_path%tests\native\TestCoreApi.vcxproj" /p:Configuration=Debug /p:Platform=Win32
+MSBuild.exe "%root_path%tests\ebm_native_api_test\ebm_native_api_test.vcxproj" /p:Configuration=Debug /p:Platform=Win32
 IF %ERRORLEVEL% NEQ 0 (
    ECHO MSBuild for Debug x86 returned error code %ERRORLEVEL%
    EXIT /B %ERRORLEVEL%
 )
 
-"%root_path%tmp\vs\bin\Debug\win\x64\TestCoreApi\test_core_api.exe"
+"%root_path%tmp\vs\bin\Debug\win\x64\ebm_native_api_test\ebm_native_api_test.exe"
 IF %ERRORLEVEL% NEQ 0 (
-   ECHO test_core_api.exe for Debug x64 failed with error code %ERRORLEVEL%
+   ECHO ebm_native_api_test.exe for Debug x64 failed with error code %ERRORLEVEL%
    EXIT /B %ERRORLEVEL%
 )
-"%root_path%tmp\vs\bin\Debug\win\Win32\TestCoreApi\test_core_api.exe"
+"%root_path%tmp\vs\bin\Debug\win\Win32\ebm_native_api_test\ebm_native_api_test.exe"
 IF %ERRORLEVEL% NEQ 0 (
-   ECHO test_core_api.exe for Debug x86 failed with error code %ERRORLEVEL%
+   ECHO ebm_native_api_test.exe for Debug x86 failed with error code %ERRORLEVEL%
    EXIT /B %ERRORLEVEL%
 )
-"%root_path%tmp\vs\bin\Release\win\x64\TestCoreApi\test_core_api.exe"
+"%root_path%tmp\vs\bin\Release\win\x64\ebm_native_api_test\ebm_native_api_test.exe"
 IF %ERRORLEVEL% NEQ 0 (
-   ECHO test_core_api.exe for Release x64 failed with error code %ERRORLEVEL%
+   ECHO ebm_native_api_test.exe for Release x64 failed with error code %ERRORLEVEL%
    EXIT /B %ERRORLEVEL%
 )
-"%root_path%tmp\vs\bin\Release\win\Win32\TestCoreApi\test_core_api.exe"
+"%root_path%tmp\vs\bin\Release\win\Win32\ebm_native_api_test\ebm_native_api_test.exe"
 IF %ERRORLEVEL% NEQ 0 (
-   ECHO test_core_api.exe for Release x86 failed with error code %ERRORLEVEL%
+   ECHO ebm_native_api_test.exe for Release x86 failed with error code %ERRORLEVEL%
    EXIT /B %ERRORLEVEL%
 )
 
--- tests/ebm_native_api_test/ebm_native_api_test.sh
+++ tests/ebm_native_api_test/ebm_native_api_test.sh
@@ -14,13 +14,13 @@ for arg in "$@"; do
 done
 
 if [ $build_core -eq 1 ]; then
-   echo "Building Core library..."
+   echo "Building ebm_native library..."
    /bin/sh "$root_path/build.sh" -32bit
 else
-   echo "Core library NOT being built"
+   echo "ebm_native library NOT being built"
 fi
 
-compile_all="\"$root_path/tests/native/TestCoreApi.cpp\" -I\"$root_path/tests/native\" -I\"$root_path/shared/native/inc\" -std=c++11 -fpermissive -O3 -march=core2"
+compile_all="\"$root_path/tests/ebm_native_api_test/EbmNativeApiTest.cpp\" -I\"$root_path/tests/ebm_native_api_test\" -I\"$root_path/shared/ebm_native/inc\" -std=c++11 -fpermissive -O3 -march=core2"
 
 if [ "$os_type" = "Darwin" ]; then
    # reference on rpath & install_name: https://www.mikeash.com/pyblog/friday-qa-2009-11-06-linking-and-install-names.html
@@ -28,121 +28,121 @@ if [ "$os_type" = "Darwin" ]; then
 
    compile_mac="$compile_all -L\"$root_path/staging\" -Wl,-rpath,@loader_path"
 
-   echo "Compiling TestCoreApi with $clang_pp_bin for macOS release|x64"
-   [ -d "$root_path/tmp/clang/intermediate/release/mac/x64/TestCoreApi" ] || mkdir -p "$root_path/tmp/clang/intermediate/release/mac/x64/TestCoreApi"
+   echo "Compiling ebm_native_api_test with $clang_pp_bin for macOS release|x64"
+   [ -d "$root_path/tmp/clang/intermediate/release/mac/x64/ebm_native_api_test" ] || mkdir -p "$root_path/tmp/clang/intermediate/release/mac/x64/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   [ -d "$root_path/tmp/clang/bin/release/mac/x64/TestCoreApi" ] || mkdir -p "$root_path/tmp/clang/bin/release/mac/x64/TestCoreApi"
+   [ -d "$root_path/tmp/clang/bin/release/mac/x64/ebm_native_api_test" ] || mkdir -p "$root_path/tmp/clang/bin/release/mac/x64/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   compile_command="$clang_pp_bin $compile_mac -m64 -DNDEBUG -l_ebmcore_mac_x64 -o \"$root_path/tmp/clang/bin/release/mac/x64/TestCoreApi/test_core_api\" 2>&1"
+   compile_command="$clang_pp_bin $compile_mac -m64 -DNDEBUG -l_ebm_native_mac_x64 -o \"$root_path/tmp/clang/bin/release/mac/x64/ebm_native_api_test/ebm_native_api_test\" 2>&1"
    compile_out=`eval $compile_command`
    ret_code=$?
    echo -n "$compile_out"
-   echo -n "$compile_out" > "$root_path/tmp/clang/intermediate/release/mac/x64/TestCoreApi/TestCoreApi_release_mac_x64_build_log.txt"
+   echo -n "$compile_out" > "$root_path/tmp/clang/intermediate/release/mac/x64/ebm_native_api_test/ebm_native_api_test_release_mac_x64_build_log.txt"
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   cp "$root_path/staging/lib_ebmcore_mac_x64.dylib" "$root_path/tmp/clang/bin/release/mac/x64/TestCoreApi/"
+   cp "$root_path/staging/lib_ebm_native_mac_x64.dylib" "$root_path/tmp/clang/bin/release/mac/x64/ebm_native_api_test/"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   "$root_path/tmp/clang/bin/release/mac/x64/TestCoreApi/test_core_api"
+   "$root_path/tmp/clang/bin/release/mac/x64/ebm_native_api_test/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
 
-   echo "Compiling TestCoreApi with $clang_pp_bin for macOS debug|x64"
-   [ -d "$root_path/tmp/clang/intermediate/debug/mac/x64/TestCoreApi" ] || mkdir -p "$root_path/tmp/clang/intermediate/debug/mac/x64/TestCoreApi"
+   echo "Compiling ebm_native_api_test with $clang_pp_bin for macOS debug|x64"
+   [ -d "$root_path/tmp/clang/intermediate/debug/mac/x64/ebm_native_api_test" ] || mkdir -p "$root_path/tmp/clang/intermediate/debug/mac/x64/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   [ -d "$root_path/tmp/clang/bin/debug/mac/x64/TestCoreApi" ] || mkdir -p "$root_path/tmp/clang/bin/debug/mac/x64/TestCoreApi"
+   [ -d "$root_path/tmp/clang/bin/debug/mac/x64/ebm_native_api_test" ] || mkdir -p "$root_path/tmp/clang/bin/debug/mac/x64/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   compile_command="$clang_pp_bin $compile_mac -m64 -l_ebmcore_mac_x64_debug -o \"$root_path/tmp/clang/bin/debug/mac/x64/TestCoreApi/test_core_api\" 2>&1"
+   compile_command="$clang_pp_bin $compile_mac -m64 -l_ebm_native_mac_x64_debug -o \"$root_path/tmp/clang/bin/debug/mac/x64/ebm_native_api_test/ebm_native_api_test\" 2>&1"
    compile_out=`eval $compile_command`
    ret_code=$?
    echo -n "$compile_out"
-   echo -n "$compile_out" > "$root_path/tmp/clang/intermediate/debug/mac/x64/TestCoreApi/TestCoreApi_debug_mac_x64_build_log.txt"
+   echo -n "$compile_out" > "$root_path/tmp/clang/intermediate/debug/mac/x64/ebm_native_api_test/ebm_native_api_test_debug_mac_x64_build_log.txt"
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   cp "$root_path/staging/lib_ebmcore_mac_x64_debug.dylib" "$root_path/tmp/clang/bin/debug/mac/x64/TestCoreApi/"
+   cp "$root_path/staging/lib_ebm_native_mac_x64_debug.dylib" "$root_path/tmp/clang/bin/debug/mac/x64/ebm_native_api_test/"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   "$root_path/tmp/clang/bin/debug/mac/x64/TestCoreApi/test_core_api"
+   "$root_path/tmp/clang/bin/debug/mac/x64/ebm_native_api_test/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
 
-   echo "Compiling TestCoreApi with $clang_pp_bin for macOS release|x86"
-   [ -d "$root_path/tmp/clang/intermediate/release/mac/x86/TestCoreApi" ] || mkdir -p "$root_path/tmp/clang/intermediate/release/mac/x86/TestCoreApi"
+   echo "Compiling ebm_native_api_test with $clang_pp_bin for macOS release|x86"
+   [ -d "$root_path/tmp/clang/intermediate/release/mac/x86/ebm_native_api_test" ] || mkdir -p "$root_path/tmp/clang/intermediate/release/mac/x86/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   [ -d "$root_path/tmp/clang/bin/release/mac/x86/TestCoreApi" ] || mkdir -p "$root_path/tmp/clang/bin/release/mac/x86/TestCoreApi"
+   [ -d "$root_path/tmp/clang/bin/release/mac/x86/ebm_native_api_test" ] || mkdir -p "$root_path/tmp/clang/bin/release/mac/x86/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   compile_command="$clang_pp_bin $compile_mac -m32 -DNDEBUG -l_ebmcore_mac_x86 -o \"$root_path/tmp/clang/bin/release/mac/x86/TestCoreApi/test_core_api\" 2>&1"
+   compile_command="$clang_pp_bin $compile_mac -m32 -DNDEBUG -l_ebm_native_mac_x86 -o \"$root_path/tmp/clang/bin/release/mac/x86/ebm_native_api_test/ebm_native_api_test\" 2>&1"
    compile_out=`eval $compile_command`
    ret_code=$?
    echo -n "$compile_out"
-   echo -n "$compile_out" > "$root_path/tmp/clang/intermediate/release/mac/x86/TestCoreApi/TestCoreApi_release_mac_x86_build_log.txt"
+   echo -n "$compile_out" > "$root_path/tmp/clang/intermediate/release/mac/x86/ebm_native_api_test/ebm_native_api_test_release_mac_x86_build_log.txt"
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   cp "$root_path/staging/lib_ebmcore_mac_x86.dylib" "$root_path/tmp/clang/bin/release/mac/x86/TestCoreApi/"
+   cp "$root_path/staging/lib_ebm_native_mac_x86.dylib" "$root_path/tmp/clang/bin/release/mac/x86/ebm_native_api_test/"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   "$root_path/tmp/clang/bin/release/mac/x86/TestCoreApi/test_core_api"
+   "$root_path/tmp/clang/bin/release/mac/x86/ebm_native_api_test/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
 
-   echo "Compiling TestCoreApi with $clang_pp_bin for macOS debug|x86"
-   [ -d "$root_path/tmp/clang/intermediate/debug/mac/x86/TestCoreApi" ] || mkdir -p "$root_path/tmp/clang/intermediate/debug/mac/x86/TestCoreApi"
+   echo "Compiling ebm_native_api_test with $clang_pp_bin for macOS debug|x86"
+   [ -d "$root_path/tmp/clang/intermediate/debug/mac/x86/ebm_native_api_test" ] || mkdir -p "$root_path/tmp/clang/intermediate/debug/mac/x86/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   [ -d "$root_path/tmp/clang/bin/debug/mac/x86/TestCoreApi" ] || mkdir -p "$root_path/tmp/clang/bin/debug/mac/x86/TestCoreApi"
+   [ -d "$root_path/tmp/clang/bin/debug/mac/x86/ebm_native_api_test" ] || mkdir -p "$root_path/tmp/clang/bin/debug/mac/x86/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   compile_command="$clang_pp_bin $compile_mac -m32 -l_ebmcore_mac_x86_debug -o \"$root_path/tmp/clang/bin/debug/mac/x86/TestCoreApi/test_core_api\" 2>&1"
+   compile_command="$clang_pp_bin $compile_mac -m32 -l_ebm_native_mac_x86_debug -o \"$root_path/tmp/clang/bin/debug/mac/x86/ebm_native_api_test/ebm_native_api_test\" 2>&1"
    compile_out=`eval $compile_command`
    ret_code=$?
    echo -n "$compile_out"
-   echo -n "$compile_out" > "$root_path/tmp/clang/intermediate/debug/mac/x86/TestCoreApi/TestCoreApi_debug_mac_x86_build_log.txt"
+   echo -n "$compile_out" > "$root_path/tmp/clang/intermediate/debug/mac/x86/ebm_native_api_test/ebm_native_api_test_debug_mac_x86_build_log.txt"
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   cp "$root_path/staging/lib_ebmcore_mac_x86_debug.dylib" "$root_path/tmp/clang/bin/debug/mac/x86/TestCoreApi/"
+   cp "$root_path/staging/lib_ebm_native_mac_x86_debug.dylib" "$root_path/tmp/clang/bin/debug/mac/x86/ebm_native_api_test/"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   "$root_path/tmp/clang/bin/debug/mac/x86/TestCoreApi/test_core_api"
+   "$root_path/tmp/clang/bin/debug/mac/x86/ebm_native_api_test/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
@@ -150,126 +150,126 @@ if [ "$os_type" = "Darwin" ]; then
 
 elif [ "$os_type" = "Linux" ]; then
    # to cross compile for different architectures x86/x64, run the following command: sudo apt-get install g++-multilib
-   # "readelf -d <lib_filename.so>" should show library rpath:    $ORIGIN/    OR    ${ORIGIN}/    for Linux so that the console app will find the core library in the same directory as the app: https://stackoverflow.com/questions/6288206/lookup-failure-when-linking-using-rpath-and-origin
+   # "readelf -d <lib_filename.so>" should show library rpath:    $ORIGIN/    OR    ${ORIGIN}/    for Linux so that the console app will find the ebm_native library in the same directory as the app: https://stackoverflow.com/questions/6288206/lookup-failure-when-linking-using-rpath-and-origin
    # the -l<library> parameter for some reason adds a lib at the start and .so at the end
 
    compile_linux="$compile_all -L\"$root_path/staging\" -Wl,-rpath-link,\"$root_path/staging\" -Wl,-rpath,'\$ORIGIN/'"
 
-   echo "Compiling TestCoreApi with $g_pp_bin for Linux release|x64"
-   [ -d "$root_path/tmp/gcc/intermediate/release/linux/x64/TestCoreApi" ] || mkdir -p "$root_path/tmp/gcc/intermediate/release/linux/x64/TestCoreApi"
+   echo "Compiling ebm_native_api_test with $g_pp_bin for Linux release|x64"
+   [ -d "$root_path/tmp/gcc/intermediate/release/linux/x64/ebm_native_api_test" ] || mkdir -p "$root_path/tmp/gcc/intermediate/release/linux/x64/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   [ -d "$root_path/tmp/gcc/bin/release/linux/x64/TestCoreApi" ] || mkdir -p "$root_path/tmp/gcc/bin/release/linux/x64/TestCoreApi"
+   [ -d "$root_path/tmp/gcc/bin/release/linux/x64/ebm_native_api_test" ] || mkdir -p "$root_path/tmp/gcc/bin/release/linux/x64/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   compile_command="$g_pp_bin $compile_linux -m64 -DNDEBUG -l_ebmcore_linux_x64 -o \"$root_path/tmp/gcc/bin/release/linux/x64/TestCoreApi/test_core_api\" 2>&1"
+   compile_command="$g_pp_bin $compile_linux -m64 -DNDEBUG -l_ebm_native_linux_x64 -o \"$root_path/tmp/gcc/bin/release/linux/x64/ebm_native_api_test/ebm_native_api_test\" 2>&1"
    compile_out=`eval $compile_command`
    ret_code=$?
    echo -n "$compile_out"
-   echo -n "$compile_out" > "$root_path/tmp/gcc/intermediate/release/linux/x64/TestCoreApi/TestCoreApi_release_linux_x64_build_log.txt"
+   echo -n "$compile_out" > "$root_path/tmp/gcc/intermediate/release/linux/x64/ebm_native_api_test/ebm_native_api_test_release_linux_x64_build_log.txt"
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   cp "$root_path/staging/lib_ebmcore_linux_x64.so" "$root_path/tmp/gcc/bin/release/linux/x64/TestCoreApi/"
+   cp "$root_path/staging/lib_ebm_native_linux_x64.so" "$root_path/tmp/gcc/bin/release/linux/x64/ebm_native_api_test/"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   "$root_path/tmp/gcc/bin/release/linux/x64/TestCoreApi/test_core_api"
+   "$root_path/tmp/gcc/bin/release/linux/x64/ebm_native_api_test/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
 
-   echo "Compiling TestCoreApi with $g_pp_bin for Linux debug|x64"
-   [ -d "$root_path/tmp/gcc/intermediate/debug/linux/x64/TestCoreApi" ] || mkdir -p "$root_path/tmp/gcc/intermediate/debug/linux/x64/TestCoreApi"
+   echo "Compiling ebm_native_api_test with $g_pp_bin for Linux debug|x64"
+   [ -d "$root_path/tmp/gcc/intermediate/debug/linux/x64/ebm_native_api_test" ] || mkdir -p "$root_path/tmp/gcc/intermediate/debug/linux/x64/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   [ -d "$root_path/tmp/gcc/bin/debug/linux/x64/TestCoreApi" ] || mkdir -p "$root_path/tmp/gcc/bin/debug/linux/x64/TestCoreApi"
+   [ -d "$root_path/tmp/gcc/bin/debug/linux/x64/ebm_native_api_test" ] || mkdir -p "$root_path/tmp/gcc/bin/debug/linux/x64/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   compile_command="$g_pp_bin $compile_linux -m64 -l_ebmcore_linux_x64_debug -o \"$root_path/tmp/gcc/bin/debug/linux/x64/TestCoreApi/test_core_api\" 2>&1"
+   compile_command="$g_pp_bin $compile_linux -m64 -l_ebm_native_linux_x64_debug -o \"$root_path/tmp/gcc/bin/debug/linux/x64/ebm_native_api_test/ebm_native_api_test\" 2>&1"
    compile_out=`eval $compile_command`
    ret_code=$?
    echo -n "$compile_out"
-   echo -n "$compile_out" > "$root_path/tmp/gcc/intermediate/debug/linux/x64/TestCoreApi/TestCoreApi_debug_linux_x64_build_log.txt"
+   echo -n "$compile_out" > "$root_path/tmp/gcc/intermediate/debug/linux/x64/ebm_native_api_test/ebm_native_api_test_debug_linux_x64_build_log.txt"
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   cp "$root_path/staging/lib_ebmcore_linux_x64_debug.so" "$root_path/tmp/gcc/bin/debug/linux/x64/TestCoreApi/"
+   cp "$root_path/staging/lib_ebm_native_linux_x64_debug.so" "$root_path/tmp/gcc/bin/debug/linux/x64/ebm_native_api_test/"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   "$root_path/tmp/gcc/bin/debug/linux/x64/TestCoreApi/test_core_api"
+   "$root_path/tmp/gcc/bin/debug/linux/x64/ebm_native_api_test/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
 
-   echo "Compiling TestCoreApi with $g_pp_bin for Linux release|x86"
-   [ -d "$root_path/tmp/gcc/intermediate/release/linux/x86/TestCoreApi" ] || mkdir -p "$root_path/tmp/gcc/intermediate/release/linux/x86/TestCoreApi"
+   echo "Compiling ebm_native_api_test with $g_pp_bin for Linux release|x86"
+   [ -d "$root_path/tmp/gcc/intermediate/release/linux/x86/ebm_native_api_test" ] || mkdir -p "$root_path/tmp/gcc/intermediate/release/linux/x86/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   [ -d "$root_path/tmp/gcc/bin/release/linux/x86/TestCoreApi" ] || mkdir -p "$root_path/tmp/gcc/bin/release/linux/x86/TestCoreApi"
+   [ -d "$root_path/tmp/gcc/bin/release/linux/x86/ebm_native_api_test" ] || mkdir -p "$root_path/tmp/gcc/bin/release/linux/x86/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   compile_command="$g_pp_bin $compile_linux -m32 -DNDEBUG -l_ebmcore_linux_x86 -o \"$root_path/tmp/gcc/bin/release/linux/x86/TestCoreApi/test_core_api\" 2>&1"
+   compile_command="$g_pp_bin $compile_linux -m32 -DNDEBUG -l_ebm_native_linux_x86 -o \"$root_path/tmp/gcc/bin/release/linux/x86/ebm_native_api_test/ebm_native_api_test\" 2>&1"
    compile_out=`eval $compile_command`
    ret_code=$?
    echo -n "$compile_out"
-   echo -n "$compile_out" > "$root_path/tmp/gcc/intermediate/release/linux/x86/TestCoreApi/TestCoreApi_release_linux_x86_build_log.txt"
+   echo -n "$compile_out" > "$root_path/tmp/gcc/intermediate/release/linux/x86/ebm_native_api_test/ebm_native_api_test_release_linux_x86_build_log.txt"
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   cp "$root_path/staging/lib_ebmcore_linux_x86.so" "$root_path/tmp/gcc/bin/release/linux/x86/TestCoreApi/"
+   cp "$root_path/staging/lib_ebm_native_linux_x86.so" "$root_path/tmp/gcc/bin/release/linux/x86/ebm_native_api_test/"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   "$root_path/tmp/gcc/bin/release/linux/x86/TestCoreApi/test_core_api"
+   "$root_path/tmp/gcc/bin/release/linux/x86/ebm_native_api_test/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
 
-   echo "Compiling TestCoreApi with $g_pp_bin for Linux debug|x86"
-   [ -d "$root_path/tmp/gcc/intermediate/debug/linux/x86/TestCoreApi" ] || mkdir -p "$root_path/tmp/gcc/intermediate/debug/linux/x86/TestCoreApi"
+   echo "Compiling ebm_native_api_test with $g_pp_bin for Linux debug|x86"
+   [ -d "$root_path/tmp/gcc/intermediate/debug/linux/x86/ebm_native_api_test" ] || mkdir -p "$root_path/tmp/gcc/intermediate/debug/linux/x86/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   [ -d "$root_path/tmp/gcc/bin/debug/linux/x86/TestCoreApi" ] || mkdir -p "$root_path/tmp/gcc/bin/debug/linux/x86/TestCoreApi"
+   [ -d "$root_path/tmp/gcc/bin/debug/linux/x86/ebm_native_api_test" ] || mkdir -p "$root_path/tmp/gcc/bin/debug/linux/x86/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   compile_command="$g_pp_bin $compile_linux -m32 -l_ebmcore_linux_x86_debug -o \"$root_path/tmp/gcc/bin/debug/linux/x86/TestCoreApi/test_core_api\" 2>&1"
+   compile_command="$g_pp_bin $compile_linux -m32 -l_ebm_native_linux_x86_debug -o \"$root_path/tmp/gcc/bin/debug/linux/x86/ebm_native_api_test/ebm_native_api_test\" 2>&1"
    compile_out=`eval $compile_command`
    ret_code=$?
    echo -n "$compile_out"
-   echo -n "$compile_out" > "$root_path/tmp/gcc/intermediate/debug/linux/x86/TestCoreApi/TestCoreApi_debug_linux_x86_build_log.txt"
+   echo -n "$compile_out" > "$root_path/tmp/gcc/intermediate/debug/linux/x86/ebm_native_api_test/ebm_native_api_test_debug_linux_x86_build_log.txt"
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   cp "$root_path/staging/lib_ebmcore_linux_x86_debug.so" "$root_path/tmp/gcc/bin/debug/linux/x86/TestCoreApi/"
+   cp "$root_path/staging/lib_ebm_native_linux_x86_debug.so" "$root_path/tmp/gcc/bin/debug/linux/x86/ebm_native_api_test/"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
    fi
-   "$root_path/tmp/gcc/bin/debug/linux/x86/TestCoreApi/test_core_api"
+   "$root_path/tmp/gcc/bin/debug/linux/x86/ebm_native_api_test/ebm_native_api_test"
    ret_code=$?
    if [ $ret_code -ne 0 ]; then 
       exit $ret_code
