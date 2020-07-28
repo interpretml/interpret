879a137be79b7808ebe947ed754dadb1c050e2db
Interpret ML <interpretml@outlook.com>
2020-07-28 05:38:40 -0700

    Update version to prerelease.

--- python/interpret-core/setup.py
+++ python/interpret-core/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 name = "interpret-core"
 # NOTE: Version is replaced by a regex script.
-version = "0.2.0"
+version = "0.2.1-prerelease"
 long_description = """
 Core system for **the** interpret package.
 
