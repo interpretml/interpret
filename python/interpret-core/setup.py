a50b1443970c95f7a2ad96d9c08badd10653b51f
Interpret ML <interpretml@outlook.com>
2020-08-06 11:07:51 -0700

    Python 3.8 testing commit.

--- python/interpret-core/setup.py
+++ python/interpret-core/setup.py
@@ -110,6 +110,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
