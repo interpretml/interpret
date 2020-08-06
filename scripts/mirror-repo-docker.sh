bf309edfc692752f23599e995b457d3320af96c8
Interpret ML <interpretml@outlook.com>
2020-08-06 11:52:22 -0700

    Adjust mirror script to take arg.

--- scripts/mirror-repo-docker.sh
+++ scripts/mirror-repo-docker.sh
@@ -4,4 +4,4 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DEST="$( readlink -f $1 )"
 mkdir -p $DEST
-rsync -avh --exclude='node_modules' --exclude='tmp' --exclude='.git' $SCRIPT_DIR/.. /usr/staging/interpret
+rsync -avh --exclude='node_modules' --exclude='tmp' --exclude='.git' $SCRIPT_DIR/.. $DEST
