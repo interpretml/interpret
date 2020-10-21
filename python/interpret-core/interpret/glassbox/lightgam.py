03e6ec75221477a0bebb62c0b3b80fb7503b4650
InterpretML <50031429+interpret-ml@users.noreply.github.com>
2020-10-21 15:55:34 -0700

    Updated data conversion methods in LightGAM.
    Co-Authored-By: S Jenkins <69673492+nopdive@users.noreply.github.com>
    Co-Authored-By: Harsha-Nori <5302119+Harsha-Nori@users.noreply.github.com>

--- python/interpret-core/interpret/glassbox/lightgam.py
+++ python/interpret-core/interpret/glassbox/lightgam.py
@@ -3,36 +3,35 @@
 
 import pandas as pd
 import numpy as np
+from ..utils import unify_data
 from joblib import Parallel, delayed
 from sklearn.utils.extmath import softmax
 from sklearn.model_selection import train_test_split
 from sklearn.base import BaseEstimator, RegressorMixin, ClassifierMixin
 
-# TODO: Move into fit method.
 
-
-def _convert_to_category(X, cat_features):
+def _convert_to_category(X, feature_names, feature_types):
     if not isinstance(X, pd.DataFrame):
         X = pd.DataFrame(X)
 
-    if cat_features == 'auto':
-        is_cat = np.array([dt.kind == 'O' for dt in X.dtypes])
-        cat_cols = X.columns.values[is_cat]
-    elif isinstance(cat_features, list) and len(cat_features) > 0 and isinstance(cat_features[0], int):
-        # Convert indexes to feature names for categorical conversion
-        cat_cols = [X.columns[index] for index in cat_features]
-    else:
-        cat_cols = None
+    cat_cols = [
+        feature_names[index] for index, ftype in enumerate(feature_types)
+        if ftype == "categorical"
+    ]
 
     # Convert to categoricals
-    if cat_cols is not None:
+    if len(cat_cols) > 0:
         for col in cat_cols:
             X[col] = pd.Categorical(X[col])
     return X
 
 
-def _fit(estimator, X, y, random_state, holdout_split, early_stopping_rounds, cat_features):
-    X_cat = _convert_to_category(X, cat_features)
+def _fit(estimator, X, y, random_state, holdout_split, early_stopping_rounds,
+         feature_names, feature_types):
+    X, y, feature_names, feature_types = unify_data(
+        X, y, feature_names, feature_types
+    )
+    X_cat = _convert_to_category(X, feature_names, feature_types)
 
     X_train, X_val, y_train, y_val = train_test_split(
         X_cat, y,
@@ -48,14 +47,14 @@ def _fit(estimator, X, y, random_state, holdout_split, early_stopping_rounds, ca
 
 
 class LightGAMRegressor(BaseEstimator, RegressorMixin):
-    def __init__(self, cat_features=None, feature_names='auto',
+    def __init__(self, feature_names=None, feature_types=None,
                  holdout_split=0.15, early_stopping_rounds=50,
                  max_depth=1, min_child_samples=2, num_leaves=3, max_rounds=5000,
                  learning_rate=0.01, colsample_bytree=0.00001, outer_bags=16,
                  n_jobs=-1, random_state=1):
 
-        self.cat_features = cat_features
         self.feature_names = feature_names
+        self.feature_types = feature_types
         self.holdout_split = holdout_split
         self.early_stopping_rounds = early_stopping_rounds
         self.max_depth = max_depth
@@ -71,8 +70,6 @@ class LightGAMRegressor(BaseEstimator, RegressorMixin):
     def fit(self, X, y):
         from lightgbm import LGBMRegressor
 
-        cat_features = self.cat_features if self.cat_features is not None else []
-
         if self.outer_bags > 1:
             estimators = []
             for bag in range(self.outer_bags):
@@ -87,7 +84,8 @@ class LightGAMRegressor(BaseEstimator, RegressorMixin):
 
             self.bagged_estimators_ = Parallel(n_jobs=self.n_jobs)(
                 delayed(_fit)(estimator, X, y, self.random_state + i, self.holdout_split, self.early_stopping_rounds,
-                              cat_features) for i, estimator in enumerate(estimators)
+                              self.feature_names, self.feature_types)
+                for i, estimator in enumerate(estimators)
             )
             return self
 
@@ -101,26 +99,27 @@ class LightGAMRegressor(BaseEstimator, RegressorMixin):
                                     verbose=-1)
 
             self.bagged_estimators_ = [
-                _fit(learner, X, y, self.random_state, self.holdout_split, self.early_stopping_rounds, cat_features)]
+                _fit(learner, X, y, self.random_state, self.holdout_split, self.early_stopping_rounds,
+                     self.feature_names, self.feature_types)]
             return self
 
     # TODO: Introduce a merge operator to build GAM graphs
 
     def predict(self, X):
-        X_cat = _convert_to_category(X, self.cat_features)
+        X_cat = _convert_to_category(X, self.feature_names, self.feature_types)
         # Should be predicting from consolidated GAM, not many bagged estimators
         return np.mean([i.predict(X_cat) for i in self.bagged_estimators_], axis=0)
 
 
 class LightGAMClassifier(BaseEstimator, ClassifierMixin):
-    def __init__(self, cat_features='auto', feature_names='auto',
+    def __init__(self, feature_names=None, feature_types=None,
                  holdout_split=0.15, early_stopping_rounds=50,
                  max_depth=1, min_child_samples=2, num_leaves=3, max_rounds=5000,
                  learning_rate=0.01, colsample_bytree=0.00001, outer_bags=16,
                  n_jobs=-1, random_state=1):
 
-        self.cat_features = cat_features
         self.feature_names = feature_names
+        self.feature_types = feature_types
         self.holdout_split = holdout_split
         self.early_stopping_rounds = early_stopping_rounds
         self.max_depth = max_depth
@@ -136,7 +135,6 @@ class LightGAMClassifier(BaseEstimator, ClassifierMixin):
     def fit(self, X, y):
         from lightgbm import LGBMClassifier
 
-        cat_features = self.cat_features if self.cat_features is not None else []
         self.classes_, y = np.unique(y, return_inverse=True)
 
         if self.outer_bags > 1:
@@ -153,7 +151,8 @@ class LightGAMClassifier(BaseEstimator, ClassifierMixin):
 
             self.estimators_ = Parallel(n_jobs=self.n_jobs)(
                 delayed(_fit)(estimator, X, y, self.random_state + i, self.holdout_split, self.early_stopping_rounds,
-                              cat_features) for i, estimator in enumerate(estimators)
+                              self.feature_names, self.feature_types)
+                for i, estimator in enumerate(estimators)
             )
             return self
 
@@ -167,11 +166,12 @@ class LightGAMClassifier(BaseEstimator, ClassifierMixin):
                                      verbose=-1)
 
             self.estimators_ = [
-                _fit(learner, X, y, self.random_state, self.holdout_split, self.early_stopping_rounds, cat_features)]
+                _fit(learner, X, y, self.random_state, self.holdout_split, self.early_stopping_rounds,
+                     self.feature_names, self.feature_types)]
             return self
 
     def predict_proba(self, X):
-        X_cat = _convert_to_category(X, self.cat_features)
+        X_cat = _convert_to_category(X, self.feature_names, self.feature_types)
         # TODO: Verify this can work for alternative loss functions
         raw_scores_vector = np.mean([i.predict_proba(X_cat, raw_score=True) for i in self.estimators_], axis=0)
 
@@ -181,6 +181,6 @@ class LightGAMClassifier(BaseEstimator, ClassifierMixin):
         return softmax(raw_scores_vector)
 
     def predict(self, X):
-        X_cat = _convert_to_category(X, self.cat_features)
+        X_cat = _convert_to_category(X, self.feature_names, self.feature_types)
         preds = self.predict_proba(X_cat)
         return self.classes_[np.argmax(preds, axis=1)]
