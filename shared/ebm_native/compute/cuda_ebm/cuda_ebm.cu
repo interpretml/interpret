adf1e0bd7f95fa794b4d5e5184046d2754407056
Paul Koch <code@koch.ninja>
2021-03-28 00:10:53 -0700

    template the gpu kernel function

--- shared/ebm_native/compute/cuda_ebm/cuda_ebm.cu
+++ shared/ebm_native/compute/cuda_ebm/cuda_ebm.cu
@@ -2,14 +2,29 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-__global__ class Addme {
+#include <type_traits>
 
+struct TestLoss {
+   float m_multiple;
+
+   TestLoss(float multiple) {
+      m_multiple = multiple;
+   }
+
+   __device__ float CalculateGradient(float target, float prediction) {
+      return target * m_multiple + prediction * 100;
+   }
+
+   // if the loss function doesn't have a second derivative, then delete the CalculateHessian function.
+   __device__ float CalculateHessian(float target, float prediction) {
+      return target * 10 + prediction * 100;
+   }
 };
 
-__global__ void TestGpuAdd(const int * const pVal1, const int * const pVal2, int * const pResult) {
-   Addme();
+template <typename TLoss>
+__global__ void TestGpuAdd(TLoss * pLoss, const int * const pVal1, const int * const pVal2, int * const pResult) {
    const size_t iGpuThread = threadIdx.x;
-   pResult[iGpuThread] = pVal1[iGpuThread] + pVal2[iGpuThread];
+   pResult[iGpuThread] = pLoss->CalculateGradient(pVal1[iGpuThread], pVal2[iGpuThread]);
 }
 
 constexpr size_t k_cItems = 5;
@@ -22,9 +37,23 @@ bool TestCuda() {
    int aResult[k_cItems];
    memset(aResult, 0, sizeof(aResult));
 
+   // TODO: unfortunately, I think this means our Loss classes need to be standard_layout and trivially copyable
+   // which means no virtual function  :(.  We can use function pointers instead though, even though that's kind
+   // of uggly, but at least those will be hidden from the Loss class writer.  In Registration.hpp after
+   // calling new TRegistrable... we still have the specific loss type after that call, so we can take a pointer
+   // to a function that we inject via the loss MACRO.  Dirty, but it'll get the job done.
+
+   static_assert(std::is_standard_layout<TestLoss>::value,
+      "Our Loss type must be a standard layout struct to be inserted into the GPU");
+   static_assert(std::is_trivially_copyable<TestLoss>::value,
+      "Our Loss type must be a trivial struct to be inserted into the GPU");
+
+   TestLoss loss(9);
+
    int * aDeviceVal1 = nullptr;
    int * aDeviceVal2 = nullptr;
    int * aDeviceResult = nullptr;
+   TestLoss * pDeviceLoss = nullptr;
    cudaError_t error;
 
    error = cudaSetDevice(0);
@@ -47,6 +76,11 @@ bool TestCuda() {
       goto exit_error;
    }
 
+   error = cudaMalloc((void **)&pDeviceLoss, sizeof(TestLoss));
+   if(cudaSuccess != error) {
+      goto exit_error;
+   }
+
    error = cudaMemcpy(aDeviceVal1, aVal1, k_cItems * sizeof(int), cudaMemcpyHostToDevice);
    if(cudaSuccess != error) {
       goto exit_error;
@@ -57,7 +91,12 @@ bool TestCuda() {
       goto exit_error;
    }
 
-   TestGpuAdd<<<1, k_cItems>>>(aDeviceVal1, aDeviceVal2, aDeviceResult);
+   error = cudaMemcpy(pDeviceLoss, &loss, sizeof(TestLoss), cudaMemcpyHostToDevice);
+   if(cudaSuccess != error) {
+      goto exit_error;
+   }
+
+   TestGpuAdd<TestLoss><<<1, k_cItems>>>(pDeviceLoss, aDeviceVal1, aDeviceVal2, aDeviceResult);
 
    error = cudaGetLastError();
    if(cudaSuccess != error) {
@@ -79,6 +118,13 @@ bool TestCuda() {
 exit_error:
 
    bool bExitHard = false;
+
+   if(nullptr != pDeviceLoss) {
+      error = cudaFree(pDeviceLoss);
+      if(cudaSuccess != error) {
+         bExitHard = true;
+      }
+   }
 
    if(nullptr != aDeviceResult) {
       error = cudaFree(aDeviceResult);
