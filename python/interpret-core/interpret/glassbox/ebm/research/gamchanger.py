913786e3f2d6461479cd4134050d68700f8ddc7c
Jay Wong <xiao.hk1997@gmail.com>
2021-07-26 13:25:30 -0400

    Add get_edited_model() for continuous variable
    Signed-off-by: Jay Wong <xiao.hk1997@gmail.com>

--- python/interpret-core/interpret/glassbox/ebm/research/gamchanger.py
+++ python/interpret-core/interpret/glassbox/ebm/research/gamchanger.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 from copy import deepcopy
-from json import dump
+from json import dump, load
 
 def get_model_data(ebm):
     """
@@ -194,3 +194,152 @@ def get_sample_data(ebm, x_test, y_test):
     }
 
     return sample_data
+
+
+def overwrite_bin_definition(ebm, index_id, new_bins, new_scores):
+    """
+    Overwrite the bin definitions and scores for continuous variables.
+    
+    Args:
+        ebm: EBM object
+        index_id: Feature's index id in the ebm object
+        new_bins: New bin definition
+        new_score: New bin scores
+        
+    In python, to overwrite the bins, we want to overwrite pair
+    `edge[:] with score[2:]` and pair `col_min_ with score [1]`.
+
+    In GAM Changer and EBM.JS, stored bins are `python_label[:-1]` and `python_score[1:]`
+
+    To map GAM Changer and EBM.JS's `newBins`, `newScores` back to Python:
+
+    ```
+    newBins[0] => col_min_
+    newBins[1:] => col_bin_edges_
+
+    newScores[:] => additive_terms_[1:]
+    ```
+    
+    We also want to update the standard deviation information:
+    
+    Case 1: Bin definition has not changed:
+        We zero out the SDs of bins that have been modified
+    
+    Case 2: Bin definition has changed (even just a subset):
+        We zero out all the SDs of bins
+        
+    In Python, SDs share the same index as scores.
+    """
+
+    assert(len(new_bins) == len(new_scores))
+
+    # Check if GAM Changer has changed the bin definition
+    binDefChanged = False
+
+    if len(new_bins) - 1 != len(ebm.preprocessor_.col_bin_edges_[index_id]):
+        binDefChanged = True
+
+    else:
+        for i in range(1, len(new_bins)):
+            if new_bins[i] != round(ebm.preprocessor_.col_bin_edges_[index_id][i - 1], 4):
+                binDefChanged = True
+                break
+
+    # Update the SDs
+    if binDefChanged:
+        ebm.term_standard_deviations_[index_id] = np.zeros(len(new_scores) + 1)
+    else:
+        # Itereate through the scores to zero out SDs of modified bins
+        for i in range(1, len(ebm.additive_terms_[index_id])):
+            if round(ebm.additive_terms_[index_id][i], 4) != new_scores[i - 1]:
+                ebm.term_standard_deviations_[index_id][i] = 0
+
+    # Overwrite the scores
+    ebm.additive_terms_[index_id] = np.array(
+        [ebm.additive_terms_[index_id][0]] + new_scores
+    ).astype(np.float64)
+
+    # Overwrite the bin edges
+
+    # GAM Changer won't change the edge for col_min_, because it
+    # will always be one of the end points in any interpolations
+    # So we don't really need to change col_min_, change here for testing purpose
+    ebm.preprocessor_.col_min_[index_id] = new_bins[0]
+    ebm.preprocessor_.col_bin_edges_[index_id] = np.array(
+        new_bins[1:]).astype(np.float64)
+
+
+
+def get_edited_model(ebm, history_filepath):
+    """
+    Return a copy of ebm that is modified based on the edits from GAM Changer.
+
+    Args:
+        ebm: EBM object
+        history_filepath: Filepath to GAM Changer's export file
+
+    Returns:
+        An edited deep copy of ebm object.
+    """
+
+    ebm_copy = deepcopy(ebm)
+
+    edit_data = load(open(history_filepath, 'r'))
+    history = edit_data['historyList']
+
+    # Mapping from feature name to feature type
+    feature_name_to_type = dict(zip(ebm_copy.feature_names, ebm_copy.feature_types))
+
+    # Keep track which feature has been updated in ebm_copy
+    updated_features = set()
+
+    # We iterate through the history list from the newest edit to the oldes edit
+    # For each modified feature, we overwrite the bin definitions/scores on an EBM
+    # copy using the latest edit info on that feature.
+    # Note that GAM Changer can only change the bin definitions of continuous features
+
+    for i in range(len(history) - 1, -1, -1):
+        cur_history = history[i]
+        print(cur_history['type'], cur_history['featureName'])
+
+        # Original edit does not change the graph
+        if cur_history['type'] == 'original':
+            continue
+
+        cur_name = cur_history['featureName']
+        cur_index = ebm_copy.feature_names.index(cur_name)
+
+        # If we have already updated EBM on this feature, skip earlier edits
+        if cur_name in updated_features:
+            continue
+
+        if feature_name_to_type[cur_name] == 'continuous':
+            # Collect bin edges and scores
+            bin_data = cur_history['state']['pointData']
+            bin_edges, bin_scores = [], []
+
+            # bin_data is a linked list, bin_data[0] is gauranteed to be the start
+            # point of all bins
+            cur_bin = bin_data['0']
+
+            while cur_bin['rightPointID']:
+                bin_edges.append(cur_bin['x'])
+                bin_scores.append(cur_bin['y'])
+                cur_bin = bin_data[str(cur_bin['rightPointID'])]
+
+            # Handle the last bin
+            bin_edges.append(cur_bin['x'])
+            bin_scores.append(cur_bin['y'])
+
+            assert(len(bin_edges) == len(bin_data))
+
+            # Overwrite EBM bin defintions/additive terms with bin_edges and bin_scores
+            overwrite_bin_definition(ebm_copy, cur_index, bin_edges, bin_scores)
+            updated_features.add(cur_name)
+
+        elif feature_name_to_type[cur_name] == 'categorical':
+            pass
+        elif feature_name_to_type[cur_name] == 'interaction':
+            pass
+        else:
+            raise ValueError('Encounter unknown feature type {}'.format(feature_name_to_type[cur_name]))
