7a13e5a73b219f8d62568584c4c37d7468bb2852
S Jenkins <nopdive@gmail.com>
2022-05-18 15:38:11 -0700

    Updated frontend tests.
    Frontend tests now set to work with new state definitions for StitchWidget.

--- python/stitch/src/__tests__/index.spec.ts
+++ python/stitch/src/__tests__/index.spec.ts
@@ -13,14 +13,32 @@ describe('Stitch', () => {
     it('should be createable', () => {
       const model = createTestModel(StitchModel);
       expect(model).toBeInstanceOf(StitchModel);
-      expect(model.get('value')).toEqual('Hello World');
+      expect(model.get('kernelmsg')).toEqual('');
+      expect(model.get('clientmsg')).toEqual('');
+      expect(model.get('srcdoc')).toEqual('<p>srcdoc should be defined by the user</p>');
+      expect(model.get('initial_height')).toEqual('1px');
+      expect(model.get('initial_width')).toEqual('1px');
+      expect(model.get('initial_border')).toEqual('0');
     });
 
     it('should be createable with a value', () => {
-      const state = { value: 'Foo Bar!' };
+      const state = { srcdoc: '<h1>New html!</h1>' };
       const model = createTestModel(StitchModel, state);
       expect(model).toBeInstanceOf(StitchModel);
-      expect(model.get('value')).toEqual('Foo Bar!');
+      expect(model.get('kernelmsg')).toEqual('');
+      expect(model.get('clientmsg')).toEqual('');
+      expect(model.get('srcdoc')).toEqual('<h1>New html!</h1>');
+      expect(model.get('initial_height')).toEqual('1px');
+      expect(model.get('initial_width')).toEqual('1px');
+      expect(model.get('initial_border')).toEqual('0');
     });
   });
 });
+
+// assert w.kernelmsg == ''
+// assert w.clientmsg == ''
+// assert w.srcdoc == '<p>srcdoc should be defined by the user</p>'
+// assert w.initial_height == '1px'
+// assert w.initial_width == '1px'
+// assert w.initial_border == '0'
+// 
