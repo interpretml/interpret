b8800d7c29a8eb952729a0ea771ef3a06e9ca85c
Paul Koch <code@koch.ninja>
2023-03-19 16:14:36 -0700

    add .nojekyll file to avoid issues with github pages

--- docs/build.bat
+++ docs/build.bat
@@ -1,3 +1,5 @@
-jupyter-book build interpret_docs
-mkdir interpret_docs\_build\html\assets\images
-copy interpret_docs\assets\images\*.png interpret_docs\_build\html\assets\images /Y
+jupyter-book build .\interpret_docs\
+mkdir .\interpret_docs\_build\html\assets\images\
+copy .\interpret_docs\assets\images\*.png .\interpret_docs\_build\html\assets\images\ /Y
+
+echo This file stops Github Pages from deleting directories that start with _> .\interpret_docs\_build\html\.nojekyll
