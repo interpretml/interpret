908d0b68f05f059e8281bde76600fb55015076f4
Paul Koch <code@koch.ninja>
2023-04-11 03:36:55 -0700

    move glassbox backing classes to private python files

--- python/interpret-core/src/interpret/glassbox/__init__.py
+++ python/interpret-core/src/interpret/glassbox/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2023 The InterpretML Contributors
 # Distributed under the MIT software license
 
-from .decisiontree import ClassificationTree, RegressionTree  # noqa: F401
-from .linear import LogisticRegression, LinearRegression  # noqa: F401
-from .skoperules import DecisionListClassifier  # noqa: F401
+from ._decisiontree import ClassificationTree, RegressionTree  # noqa: F401
+from ._linear import LogisticRegression, LinearRegression  # noqa: F401
+from ._skoperules import DecisionListClassifier  # noqa: F401
 from .ebm._ebm import ExplainableBoostingClassifier  # noqa: F401
 from .ebm._ebm import ExplainableBoostingRegressor  # noqa: F401
