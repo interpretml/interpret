02bfa9b41549655846515547314e33aac78b754c
Paul Koch <code@koch.ninja>
2023-04-12 23:31:33 -0700

    move greybox explainers into private submodules

--- python/interpret-core/src/interpret/greybox/__init__.py
+++ python/interpret-core/src/interpret/greybox/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2023 The InterpretML Contributors
 # Distributed under the MIT software license
 
-from .treeinterpreter import TreeInterpreter  # noqa: F401
-from .shaptree import ShapTree  # noqa: F401
+from ._treeinterpreter import TreeInterpreter  # noqa: F401
+from ._shaptree import ShapTree  # noqa: F401
