334d050b97e0d7db2ab75eaa5871811511bc41d3
Paul Koch <code@koch.ninja>
2023-04-12 23:34:04 -0700

    move perf into private submodules

--- python/interpret-core/src/interpret/perf/__init__.py
+++ python/interpret-core/src/interpret/perf/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2023 The InterpretML Contributors
 # Distributed under the MIT software license
 
-from .curve import ROC, PR  # noqa: F401
-from .regression import RegressionPerf  # noqa: F401
+from ._curve import ROC, PR  # noqa: F401
+from ._regression import RegressionPerf  # noqa: F401
