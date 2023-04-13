8f1e3ae93c900c0aa4b5ce5082c4e37fe9291e25
Paul Koch <code@koch.ninja>
2023-04-12 23:23:53 -0700

    put blackbox explainers into private submodules

--- python/interpret-core/src/interpret/blackbox/__init__.py
+++ python/interpret-core/src/interpret/blackbox/__init__.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2023 The InterpretML Contributors
 # Distributed under the MIT software license
 
-from .lime import LimeTabular  # noqa: F401
-from .shap import ShapKernel  # noqa: F401
-from .sensitivity import MorrisSensitivity  # noqa: F401
-from .partialdependence import PartialDependence  # noqa: F401
+from ._lime import LimeTabular  # noqa: F401
+from ._shap import ShapKernel  # noqa: F401
+from ._sensitivity import MorrisSensitivity  # noqa: F401
+from ._partialdependence import PartialDependence  # noqa: F401
 
 # from .permutationimportance import PermutationImportance  # noqa: F401
