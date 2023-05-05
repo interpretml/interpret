9339cd2d81840ef5286c8101f830a91e554f68a5
mtl-tony <yuan.tony.1@gmail.com>
2023-05-04 23:45:29 -0400

    Tweedie Deviance
    Signed-off-by: mtl-tony <yuan.tony.1@gmail.com>

--- shared/libebm/compute/objectives/TweedieDevianceRegressionObjective.hpp
+++ shared/libebm/compute/objectives/TweedieDevianceRegressionObjective.hpp
@@ -7,28 +7,33 @@
 // TFloat is a datatype that could hold inside a double, float, or some SIMD intrinsic type.
 // See sse2_32.cpp, cuda_32.cpp, and cpu_64.cpp as examples where TFloat operators are defined.
 template<typename TFloat>
-struct PoissonDevianceRegressionObjective : RegressionObjective {
-   OBJECTIVE_BOILERPLATE(PoissonDevianceRegressionObjective, MINIMIZE_METRIC, Link_log)
+struct TweedieDevianceRegressionObjective : RegressionObjective {
+   OBJECTIVE_BOILERPLATE(TweedieDevianceRegressionObjective, MINIMIZE_METRIC, Link_log)
 
-   static constexpr double epsilon = 1e-8;
+   double m_variancePowerParam;
+   double m_variancePowerParamSub1;
+   double m_variancePowerParamSub2;
 
    // The constructor parameters following config must match the RegisterObjective parameters in objective_registrations.hpp
-   inline PoissonDevianceRegressionObjective(const Config & config) {
-      // XGBoost and LightGBM have a max_delta_step parameter which is set to 0.7 by default. This parameter's only
-      // actual effect is that it changes the effective learning rate by a multiple of 1/e^max_delta_step, which
-      // is a parameter that you can set already from the public interface.
-      // They add this parameter value to the prediction before taking the exp for the hessian calculation, but you can 
-      // extract it to outside the exp by multiplying the hessian by e^max_delta_step instead.
-      // To get an equivalent rate to XGBoost/LightGBM for Poisson, multiply our learning rate by
-      // 0.49658530379140951.
-
+   inline TweedieDevianceRegressionObjective(const Config & config, double variancePower) {
       if(config.cOutputs != 1) {
          throw ParamMismatchWithConfigException();
       }
-
       if(config.isDifferentiallyPrivate) {
          throw NonPrivateRegistrationException();
       }
+      if(variancePower < 1.0 || variancePower > 2.0) {
+         // TODO: Implement Tweedie for other Powers. For now skip this registration
+         throw SkipRegistrationException();
+      }
+      // for a discussion on variance_power and link_power, see:
+      // https://search.r-project.org/CRAN/refmans/statmod/html/tweedie.html
+      // https://docs.h2o.ai/h2o/latest-stable/h2o-docs/data-science/algo-params/tweedie_link_power.html
+
+      m_variancePowerParam = variancePower;
+      m_variancePowerParamSub1 = 1 - m_variancePowerParam;
+      m_variancePowerParamSub2 = 2 - m_variancePowerParam;
+
    }
 
    inline double LinkParam() const noexcept {
@@ -64,31 +69,31 @@ struct PoissonDevianceRegressionObjective : RegressionObjective {
    }
 
    inline double FinishMetric(const double metricSum) const noexcept {
-      return 2.0 * metricSum;
+      return 2*metricSum;
    }
 
-   //Different GBM Implementations
-   // https://github.com/h2oai/h2o-3/blob/master/h2o-core/src/main/java/hex/DistributionFactory.java
-   // https://github.com/microsoft/LightGBM/blob/master/src/objective/regression_objective.hpp
-   //
    GPU_DEVICE inline TFloat CalcMetric(const TFloat score, const TFloat target) const noexcept {
       const TFloat prediction = Exp(score); // log link function
-      const TFloat error = prediction - target;
-      const TFloat extra = target * Log(target / prediction);
-      const TFloat conditionalExtra = IfLess(target, epsilon, 0.0, extra);
-      return error + conditionalExtra;
+      const TFloat exp1Score = Exp((m_variancePowerParamSub1) * score);
+      const TFloat exp2Score = Exp((m_variancePowerParamSub2) * score);
+      const TFloat metric = -target * (exp1Score / m_variancePowerParamSub1)  + (exp2Score / m_variancePowerParamSub2);
+      return metric;
    }
 
    GPU_DEVICE inline TFloat CalcGradient(const TFloat score, const TFloat target) const noexcept {
       const TFloat prediction = Exp(score); // log link function
-      const TFloat gradient = prediction - target;
+      const TFloat exp1Score = Exp((m_variancePowerParamSub1) * score);
+      const TFloat exp2Score = Exp((m_variancePowerParamSub2) * score);
+      const TFloat gradient = (-target * exp1Score + exp2Score);
       return gradient;
    }
 
    GPU_DEVICE inline GradientHessian<TFloat> CalcGradientHessian(const TFloat score, const TFloat target) const noexcept {
       const TFloat prediction = Exp(score); // log link function
-      const TFloat gradient = prediction - target;
-      const TFloat hessian = prediction;
+      const TFloat exp1Score = Exp((m_variancePowerParamSub1) * score);
+      const TFloat exp2Score = Exp((m_variancePowerParamSub2) * score);
+      const TFloat gradient = (-target * exp1Score + exp2Score);
+      const TFloat hessian = (-target * (m_variancePowerParamSub1) * exp1Score + (m_variancePowerParamSub2) * exp2Score);
       return MakeGradientHessian(gradient, hessian);
    }
 };
