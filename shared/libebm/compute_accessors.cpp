9ce02b20f18543a035c240cf1013a61832c17538
Paul Koch <code@koch.ninja>
2023-07-12 12:19:43 -0700

    add SIMD version runtime checks for intel

--- shared/libebm/compute_accessors.cpp
+++ shared/libebm/compute_accessors.cpp
@@ -6,6 +6,21 @@
 
 #include <stddef.h> // size_t, ptrdiff_t
 
+#if defined(BRIDGE_SSE2_32) || defined(BRIDGE_AVX512_32)
+#define INTEL_SIMD
+#endif
+
+#ifdef INTEL_SIMD
+
+#ifdef _MSC_VER
+#include <intrin.h>
+#else // compiler type
+// clang or gcc
+#include <x86intrin.h>
+#endif // compiler type
+
+#endif // INTEL_SIMD
+
 #include "libebm.h" // ErrorEbm
 #include "logging.h" // EBM_ASSERT
 #include "common_c.h"
@@ -18,6 +33,115 @@ namespace DEFINED_ZONE_NAME {
 #ifndef DEFINED_ZONE_NAME
 #error DEFINED_ZONE_NAME must be defined
 #endif // DEFINED_ZONE_NAME
+
+#ifdef INTEL_SIMD
+
+inline static void cpuid(int cpuInfo[4], int function_id, int subfunction_id = 0) {
+   // from: https://github.com/vectorclass/version2/blob/9d324e13457cf67b44be04f49a4a0036bb188a89/instrset.h#L217
+#if defined (_MSC_VER)
+   __cpuidex(cpuInfo, function_id, subfunction_id);
+#elif defined(__GNUC__) || defined(__clang__)
+   int a, b, c, d;
+   __asm("cpuid" : "=a"(a), "=b"(b), "=c"(c), "=d"(d) : "a"(function_id), "c"(subfunction_id) : );
+   cpuInfo[0] = a;
+   cpuInfo[1] = b;
+   cpuInfo[2] = c;
+   cpuInfo[3] = d;
+#else
+   // Some other platform. Try masm/intel syntax assembly
+   __asm {
+      mov eax, function_id
+      mov ecx, subfunction_id
+      cpuid;
+      mov esi, cpuInfo
+      mov[esi], eax
+      mov[esi + 4], ebx
+      mov[esi + 8], ecx
+      mov[esi + 12], edx
+   }
+#endif
+}
+
+inline static uint64_t xgetbv(int xcr) {
+   // from: https://github.com/vectorclass/version2/blob/9d324e13457cf67b44be04f49a4a0036bb188a89/instrset_detect.cpp#L22C1-L48C1
+#if (defined (_MSC_FULL_VER) && _MSC_FULL_VER >= 160040000) || (defined (__INTEL_COMPILER) && __INTEL_COMPILER >= 1200)
+   return uint64_t(_xgetbv(xcr));
+#elif defined(__GNUC__) ||  defined (__clang__)
+   uint32_t a, d;
+   __asm("xgetbv" : "=a"(a), "=d"(d) : "c"(xcr) : );
+   return a | (uint64_t(d) << 32);
+#else
+   // Some other platform. Try masm/intel syntax assembly
+   uint32_t a, d;
+   __asm {
+      mov ecx, xcr
+      _emit 0x0f
+      _emit 0x01
+      _emit 0xd0; // xgetbv
+      mov a, eax
+      mov d, edx
+   }
+   return a | (uint64_t(d) << 32);
+#endif
+}
+
+static int DetectInstructionset(void) {
+   // from: https://github.com/vectorclass/version2/blob/9d324e13457cf67b44be04f49a4a0036bb188a89/instrset_detect.cpp#L63
+   static int instructionSet = -1;
+   if(instructionSet >= 0) {
+      return instructionSet;
+   }
+   instructionSet = 0;
+   int abcd[4] = { 0, 0, 0, 0 };
+   cpuid(abcd, 0);
+   if(abcd[0] == 0) return instructionSet;
+   cpuid(abcd, 1);
+   if((abcd[3] & (1 << 0)) == 0) return instructionSet;
+   if((abcd[3] & (1 << 23)) == 0) return instructionSet;
+   if((abcd[3] & (1 << 15)) == 0) return instructionSet;
+   if((abcd[3] & (1 << 24)) == 0) return instructionSet;
+   if((abcd[3] & (1 << 25)) == 0) return instructionSet;
+   instructionSet = 1;
+   if((abcd[3] & (1 << 26)) == 0) return instructionSet;
+   instructionSet = 2;
+   if((abcd[2] & (1 << 0)) == 0) return instructionSet;
+   instructionSet = 3;
+   if((abcd[2] & (1 << 9)) == 0) return instructionSet;
+   instructionSet = 4;
+   if((abcd[2] & (1 << 19)) == 0) return instructionSet;
+   instructionSet = 5;
+   if((abcd[2] & (1 << 23)) == 0) return instructionSet;
+   if((abcd[2] & (1 << 20)) == 0) return instructionSet;
+   instructionSet = 6;
+   if((abcd[2] & (1 << 27)) == 0) return instructionSet;
+   if((xgetbv(0) & 6) != 6)       return instructionSet;
+   if((abcd[2] & (1 << 28)) == 0) return instructionSet;
+   instructionSet = 7;
+   cpuid(abcd, 7);
+   if((abcd[1] & (1 << 5)) == 0) return instructionSet;
+   instructionSet = 8;
+   if((abcd[1] & (1 << 16)) == 0) return instructionSet;
+   cpuid(abcd, 0xD);
+   if((abcd[0] & 0x60) != 0x60)   return instructionSet;
+   instructionSet = 9;
+   cpuid(abcd, 7);
+   if((abcd[1] & (1 << 31)) == 0) return instructionSet;
+   if((abcd[1] & 0x40020000) != 0x40020000) return instructionSet;
+   instructionSet = 10;
+   return instructionSet;
+}
+
+inline static bool IsAvx512f() {
+   return DetectInstructionset() >= 9;
+}
+
+#else // INTEL_SIMD
+
+inline constexpr static bool IsAvx512f() {
+   return false;
+}
+
+#endif // INTEL_SIMD
 
 extern ErrorEbm GetObjective(
    const Config * const pConfig,
@@ -55,16 +179,14 @@ extern ErrorEbm GetObjective(
       //       we first make the cpu version and if that says it can't be SIMDed then we shouldn't try
       while(true) {
 #ifdef BRIDGE_AVX512_32
-
-         // TODO: dynamically check for AVX-512, and also enable BRIDGE_AVX512_32 in our compile to
-         // make the checks happen
-
-         LOG_0(Trace_Info, "INFO GetObjective creating AVX512 SIMD Objective");
-         error = CreateObjective_Avx512_32(pConfig, sObjective, sObjectiveEnd, pSIMDObjectiveWrapperOut);
-         if(Error_None != error) {
-            return error;
+         if(IsAvx512f()) {
+            LOG_0(Trace_Info, "INFO GetObjective creating AVX512 SIMD Objective");
+            error = CreateObjective_Avx512_32(pConfig, sObjective, sObjectiveEnd, pSIMDObjectiveWrapperOut);
+            if(Error_None != error) {
+               return error;
+            }
+            break;
          }
-         break;
 #endif // BRIDGE_AVX512_32
 
 #ifdef BRIDGE_SSE2_32
@@ -81,6 +203,8 @@ extern ErrorEbm GetObjective(
    return Error_None;
 }
 
+#ifdef NEVER
+// TODO: eventually enable metrics
 INLINE_RELEASE_UNTEMPLATED static ErrorEbm GetMetrics(
    const Config * const pConfig,
    const char * sMetric
@@ -123,5 +247,6 @@ INLINE_RELEASE_UNTEMPLATED static ErrorEbm GetMetrics(
       sMetric = sMetricEnd + 1;
    }
 }
+#endif // NEVER
 
 } // DEFINED_ZONE_NAME
