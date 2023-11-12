8cecfc4a7491a307d76c6db4b281634de0b8a764
Paul Koch <code@koch.ninja>
2023-11-11 21:00:49 -0800

    separate BinSumsBoostingInternal into a version for single scores and multiclass

--- shared/libebm/compute/BinSumsBoosting.hpp
+++ shared/libebm/compute/BinSumsBoosting.hpp
@@ -163,7 +163,200 @@ template<
    bool bWeight, 
    bool bReplication, 
    int cCompilerPack, 
-   typename std::enable_if<k_cItemsPerBitPackNone != cCompilerPack, int>::type = 0
+   typename std::enable_if<k_cItemsPerBitPackNone != cCompilerPack && 1 == cCompilerScores, int>::type = 0
+>
+GPU_DEVICE NEVER_INLINE static void BinSumsBoostingInternal(BinSumsBoostingBridge * const pParams) {
+   static_assert(bWeight || !bReplication, "bReplication cannot be true if bWeight is false");
+
+#ifndef GPU_COMPILE
+   EBM_ASSERT(nullptr != pParams);
+   EBM_ASSERT(1 <= pParams->m_cSamples);
+   EBM_ASSERT(0 == pParams->m_cSamples % size_t { TFloat::k_cSIMDPack });
+   EBM_ASSERT(nullptr != pParams->m_aGradientsAndHessians);
+   EBM_ASSERT(nullptr != pParams->m_aFastBins);
+   EBM_ASSERT(size_t { 1 } == pParams->m_cScores);
+#endif // GPU_COMPILE
+
+   auto * const aBins = reinterpret_cast<BinBase *>(pParams->m_aFastBins)->Specialize<typename TFloat::T, typename TFloat::TInt::T, bHessian, size_t { 1 }>();
+
+   const size_t cSamples = pParams->m_cSamples;
+
+   const typename TFloat::T * pGradientAndHessian = reinterpret_cast<const typename TFloat::T *>(pParams->m_aGradientsAndHessians);
+   const typename TFloat::T * const pGradientsAndHessiansEnd = pGradientAndHessian + (bHessian ? size_t { 2 } : size_t { 1 }) * cSamples;
+
+   typename TFloat::TInt::T cBytesPerBin;
+   int cBitsPerItemMax;
+   int cShift;
+   int cShiftReset;
+   typename TFloat::TInt maskBits;
+   const typename TFloat::TInt::T * pInputData;
+
+   cBytesPerBin = static_cast<typename TFloat::TInt::T>(GetBinSize<typename TFloat::T, typename TFloat::TInt::T>(bHessian, size_t { 1 }));
+
+   const int cItemsPerBitPack = GET_ITEMS_PER_BIT_PACK(cCompilerPack, pParams->m_cPack);
+#ifndef GPU_COMPILE
+   EBM_ASSERT(k_cItemsPerBitPackNone != cItemsPerBitPack); // we require this condition to be templated
+   EBM_ASSERT(1 <= cItemsPerBitPack);
+   EBM_ASSERT(cItemsPerBitPack <= COUNT_BITS(typename TFloat::TInt::T));
+#endif // GPU_COMPILE
+
+   cBitsPerItemMax = GetCountBits<typename TFloat::TInt::T>(cItemsPerBitPack);
+#ifndef GPU_COMPILE
+   EBM_ASSERT(1 <= cBitsPerItemMax);
+   EBM_ASSERT(cBitsPerItemMax <= COUNT_BITS(typename TFloat::TInt::T));
+#endif // GPU_COMPILE
+
+   cShift = static_cast<int>(((cSamples >> TFloat::k_cSIMDShift) - size_t { 1 }) % static_cast<size_t>(cItemsPerBitPack)) * cBitsPerItemMax;
+   cShiftReset = (cItemsPerBitPack - 1) * cBitsPerItemMax;
+
+   maskBits = MakeLowMask<typename TFloat::TInt::T>(cBitsPerItemMax);
+
+   pInputData = reinterpret_cast<const typename TFloat::TInt::T *>(pParams->m_aPacked);
+#ifndef GPU_COMPILE
+   EBM_ASSERT(nullptr != pInputData);
+#endif // GPU_COMPILE
+
+   const typename TFloat::T * pWeight;
+   const uint8_t * pCountOccurrences;
+   if(bWeight) {
+      pWeight = reinterpret_cast<const typename TFloat::T *>(pParams->m_aWeights);
+#ifndef GPU_COMPILE
+      EBM_ASSERT(nullptr != pWeight);
+#endif // GPU_COMPILE
+      if(bReplication) {
+         pCountOccurrences = pParams->m_pCountOccurrences;
+#ifndef GPU_COMPILE
+         EBM_ASSERT(nullptr != pCountOccurrences);
+#endif // GPU_COMPILE
+      }
+   }
+
+   do {
+      typename TFloat::TInt iTensorBinCombined;
+      iTensorBinCombined = TFloat::TInt::Load(pInputData);
+      pInputData += TFloat::TInt::k_cSIMDPack;
+      do {
+         Bin<typename TFloat::T, typename TFloat::TInt::T, bHessian, size_t { 1 }> * apBins[TFloat::k_cSIMDPack];
+         typename TFloat::TInt iTensorBin = (iTensorBinCombined >> cShift) & maskBits;
+            
+         // normally the compiler is better at optimimizing multiplications into shifs, but it isn't better
+         // if TFloat is a SIMD type. For SIMD shifts & adds will almost always be better than multiplication if
+         // there are low numbers of shifts, which should be the case for anything with a compile time constant here
+         iTensorBin = Multiply<typename TFloat::TInt, typename TFloat::TInt::T, 
+            1 != TFloat::k_cSIMDPack, 
+            static_cast<typename TFloat::TInt::T>(GetBinSize<typename TFloat::T, typename TFloat::TInt::T>(bHessian, size_t { 1 }))>(
+               iTensorBin, cBytesPerBin);
+            
+         TFloat::TInt::Execute([aBins, &apBins](const int i, const typename TFloat::TInt::T x) {
+            apBins[i] = IndexBin(aBins, static_cast<size_t>(x));
+         }, iTensorBin);
+#ifndef NDEBUG
+#ifndef GPU_COMPILE
+         TFloat::Execute([cBytesPerBin, apBins, pParams](const int i) {
+            ASSERT_BIN_OK(cBytesPerBin, apBins[i], pParams->m_pDebugFastBinsEnd);
+         });
+#endif // GPU_COMPILE
+#endif // NDEBUG
+
+         // TODO: the ultimate version of this algorithm would:
+         //   1) Write to k_cSIMDPack histograms simutaneously to avoid collisions of indexes
+         //   2) Sum up the final histograms using SIMD operations in parallel.  If we hvae k_cSIMDPack
+         //      histograms, then we're prefectly suited to sum them, and integers and float32 values shouldn't
+         //      have issues since we stay well away from 2^32 integers, and the float values don't have addition
+         //      issues anymore (where you can't add a 1 to more than 16 million floats)
+         //   3) Only do the above if there aren't too many bins. If we put each sample into it's own bin
+         //      for a feature, then we should prefer using this version that keeps only 1 histogram
+
+         if(bReplication) {
+            const typename TFloat::TInt cOccurences = TFloat::TInt::LoadBytes(pCountOccurrences);
+            pCountOccurrences += TFloat::k_cSIMDPack;
+
+            TFloat::TInt::Execute([apBins](const int i, const typename TFloat::TInt::T x) {
+               auto * const pBin = apBins[i];
+               // TODO: In the future we'd like to eliminate this but we need the ability to change the Bin class
+               //       such that we can remove that field optionally
+               pBin->SetCountSamples(pBin->GetCountSamples() + x);
+            }, cOccurences);
+         } else {
+            TFloat::Execute([apBins](const int i) {
+               auto * const pBin = apBins[i];
+               // TODO: In the future we'd like to eliminate this but we need the ability to change the Bin class
+               //       such that we can remove that field optionally
+               pBin->SetCountSamples(pBin->GetCountSamples() + typename TFloat::TInt::T { 1 });
+            });
+         }
+
+         TFloat weight;
+         if(bWeight) {
+            weight = TFloat::Load(pWeight);
+            pWeight += TFloat::k_cSIMDPack;
+
+            TFloat::Execute([apBins](const int i, const typename TFloat::T x) {
+               auto * const pBin = apBins[i];
+               // TODO: In the future we'd like to eliminate this but we need the ability to change the Bin class
+               //       such that we can remove that field optionally
+               pBin->SetWeight(pBin->GetWeight() + x);
+            }, weight);
+         } else {
+            TFloat::Execute([apBins](const int i) {
+               auto * const pBin = apBins[i];
+               // TODO: In the future we'd like to eliminate this but we need the ability to change the Bin class
+               //       such that we can remove that field optionally
+               pBin->SetWeight(pBin->GetWeight() + typename TFloat::T { 1.0 });
+            });
+         }
+
+         if(bHessian) {
+            TFloat gradient = TFloat::Load(&pGradientAndHessian[0]);
+            TFloat hessian = TFloat::Load(&pGradientAndHessian[TFloat::k_cSIMDPack]);
+            if(bWeight) {
+               gradient *= weight;
+               hessian *= weight;
+            }
+            TFloat::Execute([apBins](const int i, const typename TFloat::T grad, const typename TFloat::T hess) {
+               // BEWARE: unless we generate a separate histogram for each SIMD stream and later merge them, pBin can 
+               // point to the same bin in multiple samples within the SIMD pack, so we need to serialize fetching sums
+               auto * const pBin = apBins[i];
+               auto * const aGradientPair = pBin->GetGradientPairs();
+               auto * const pGradientPair = &aGradientPair[0];
+               typename TFloat::T binGrad = pGradientPair->m_sumGradients;
+               typename TFloat::T binHess = pGradientPair->GetHess();
+               binGrad += grad;
+               binHess += hess;
+               pGradientPair->m_sumGradients = binGrad;
+               pGradientPair->SetHess(binHess);
+            }, gradient, hessian);
+         } else {
+            TFloat gradient = TFloat::Load(&pGradientAndHessian[0]);
+            if(bWeight) {
+               gradient *= weight;
+            }
+            TFloat::Execute([apBins](const int i, const typename TFloat::T grad) {
+               // TODO: for this special case of having just 1 bin, we could sum all the gradients and hessians
+               // before then adding them to the only bin
+               auto * const pBin = apBins[i];
+               auto * const aGradientPair = pBin->GetGradientPairs();
+               auto * const pGradientPair = &aGradientPair[0];
+               pGradientPair->m_sumGradients += grad;
+            }, gradient);
+         }
+
+         pGradientAndHessian += bHessian ? (size_t { 2 } * TFloat::k_cSIMDPack) : TFloat::k_cSIMDPack;
+
+         cShift -= cBitsPerItemMax;
+      } while(0 <= cShift);
+      cShift = cShiftReset;
+   } while(pGradientsAndHessiansEnd != pGradientAndHessian);
+}
+
+template<
+   typename TFloat, 
+   bool bHessian, 
+   size_t cCompilerScores, 
+   bool bWeight, 
+   bool bReplication, 
+   int cCompilerPack, 
+   typename std::enable_if<k_cItemsPerBitPackNone != cCompilerPack && 1 != cCompilerScores, int>::type = 0
 >
 GPU_DEVICE NEVER_INLINE static void BinSumsBoostingInternal(BinSumsBoostingBridge * const pParams) {
    static_assert(bWeight || !bReplication, "bReplication cannot be true if bWeight is false");
