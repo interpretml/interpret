1a9c3622ca36fbea74487b8d187a70fb7d2db4f0
Paul Koch <code@koch.ninja>
2023-12-26 21:52:43 -0700

    improvements to synthetic dataset generation

--- python/interpret-core/interpret/utils/_synthetic.py
+++ python/interpret-core/interpret/utils/_synthetic.py
@@ -2,14 +2,16 @@
 # Distributed under the MIT software license
 
 import numpy as np
+from itertools import takewhile
+import operator
 
 
 def _make_categorical_float(rng, n_samples, n_categories, cat_digits):
     n_modulo = 10**cat_digits
     n_categories = min(n_categories, n_modulo - 1)
     vals = rng.choice(n_categories, n_samples)
-    mapping = rng.permutation(n_categories) + 1
-    mapping = mapping[vals] + n_modulo
+    mapping = n_modulo + 1 + rng.permutation(n_categories)
+    mapping = mapping[vals]
     mapping = mapping.astype(str)
     mapping = np.char.add(mapping, ".")
     # reserve 0 for an alternative missing representation
@@ -23,16 +25,17 @@ def _make_categorical_float(rng, n_samples, n_categories, cat_digits):
 def _make_categorical_str(col, prefix, cat_digits):
     cat_mod = 10**cat_digits
 
-    order = np.floor(col).astype(int) % cat_mod
-    order = order.astype(str)
-    order = np.char.zfill(order, cat_digits)
-    order = np.char.translate(order, str.maketrans("0123456789", "abcdefghij"))
-    order = np.char.add(prefix, order)
-    order = np.char.add(order, "_")
+    order = np.floor(col)
 
-    col = np.round(col * cat_mod).astype(int) % cat_mod
-    col = col.astype(str)
-    col = np.char.zfill(col, cat_digits)
+    col -= order
+    col *= cat_mod
+    col = np.char.zfill(np.round(col).astype(int).astype(str), cat_digits)
+
+    order = order.astype(int)
+    order -= cat_mod
+    order = np.char.zfill(order.astype(str), cat_digits)
+    order = np.char.translate(order, str.maketrans("0123456789", "abcdefghij"))
+    order = np.char.add(prefix, np.char.add(order, "_"))
 
     return np.char.add(order, col)
 
@@ -47,7 +50,7 @@ def _synthetic_features(
     # Each feature is roughly set such that the average of the negative values is -1.0
     # and the average of the positive values is 1.0. This allows us to have a common
     # scale with integers where we have 5 categories from -2 to +2, and we clip at
-    # -2.0 and +2.0 to make transformations like exp(x) and x**2 not too extreme.
+    # -2.0 and +2.0 to make transformations like exp(x) and x**3 not too extreme.
 
     min_clip_int = int(np.ceil(min_clip))
     max_clip_int = int(np.floor(max_clip))
@@ -125,17 +128,8 @@ def _synthetic_features(
     types.append("continuous")
     features.append(np.clip(features[3] * features[4], min_clip_int, max_clip_int))
 
-    # Feature 8 - Categorical feature with high cardinality
-    names.append("f8_high")
-    types.append("nominal")
-    n_categories = int(n_samples / 4)
-    col = _make_categorical_float(rng, n_samples, n_categories, cat_digits)
-    if objects:
-        col = _make_categorical_str(col, "h", cat_digits)
-    features.append(col)
-
-    # Feature 9 - Categorical feature with low cardinality
-    names.append("f9_low")
+    # Feature 8 - Categorical feature with low cardinality
+    names.append("f8_low")
     types.append("nominal")
     n_categories = 9
     col = _make_categorical_float(rng, n_samples, n_categories, cat_digits)
@@ -143,7 +137,16 @@ def _synthetic_features(
         col = _make_categorical_str(col, "l", cat_digits)
     features.append(col)
 
-    # Convert list of features to a 2D numpy array of dtype=object and transpose
+    # Feature 9 - Categorical feature with high cardinality
+    names.append("f9_high")
+    types.append("nominal")
+    n_categories = 50
+    col = _make_categorical_float(rng, n_samples, n_categories, cat_digits)
+    if objects:
+        col = _make_categorical_str(col, "h", cat_digits)
+    features.append(col)
+
+    # Convert list of features to a 2D numpy array and transpose
     X = np.array(features, dtype=object if objects else float).T
 
     if missing:
@@ -155,32 +158,40 @@ def _synthetic_features(
 
 
 def _normalize_string_categorical(col, min_clip, max_clip):
-    missings = np.logical_or(col == np.array(None), col != col)
+    missings = col != col
     if not missings.all():
-        col[missings] = "m_0"
-        col = np.array([float(x.split("_")[1]) for x in col], float)
+        col[missings] = "_0"
+        col = col.astype(str)
+        col = np.char.rpartition(col, "_")[:, 2]
+        col = col.astype(int)
 
-        col -= col[~missings].min()
-        col_max = col[~missings].max()
-        col *= (max_clip - min_clip) / col_max
+        col = col - 1
+        multiple = (max_clip - min_clip) / float(col.max())
+        col = col.astype(float)
+        col *= multiple
         col += min_clip
 
-    col[missings] = np.nan
+        col[missings] = np.nan
+
     return col
 
 
 def _normalize_float_categorical(col, min_clip, max_clip):
     missings = np.isnan(col)
-    if not missings.all():
-        cat_digits = len(str(int(np.floor(np.nanmax(col))))) - 1
+    i_nonnan = sum(takewhile(operator.truth, missings))
+    if i_nonnan != len(col):
+        cat_digits = len(str(int(np.floor(col[i_nonnan])))) - 1
         cat_mod = 10**cat_digits
 
         col[missings] = 0.0
-        col = (np.round(col * cat_mod).astype(int) % cat_mod).astype(float)
+        col -= np.floor(col)
+        col *= cat_mod
+        col = np.round(col).astype(int)
 
-        col -= col[~missings].min()
-        col_max = col[~missings].max()
-        col *= (max_clip - min_clip) / col_max
+        col = col - 1
+        multiple = (max_clip - min_clip) / float(col.max())
+        col = col.astype(float)
+        col *= multiple
         col += min_clip
 
         col[missings] = np.nan
@@ -188,21 +199,21 @@ def _normalize_float_categorical(col, min_clip, max_clip):
 
 
 def _normalize_categoricals(X, types, min_clip, max_clip):
-    X = X.copy()
     if X.dtype == object:
-        # if we have str categoricals, convert to float and normalize their range
+        features = []
         for i in range(X.shape[1]):
             col = X[:, i]
+            col[col == np.array(None)] = np.nan
             if str in set(map(type, col)):
-                X[:, i] = _normalize_string_categorical(col, min_clip, max_clip)
-        missings = np.logical_or(X == np.array(None), X != X)
-        X[missings] = np.nan  # change any None(s) to np.nan
-        X = X.astype(float)
+                col = _normalize_string_categorical(col, min_clip, max_clip)
+            features.append(col)
+        X = np.array(features, float).T
     else:
-        # if we have float categoricals, normalize their range
-        for i in range(X.shape[1]):
-            if types is not None and types[i] == "nominal":
-                X[:, i] = _normalize_float_categorical(X[:, i], min_clip, max_clip)
+        X = X.copy()
+        if types is not None:
+            for i in range(X.shape[1]):
+                if types[i] == "nominal":
+                    X[:, i] = _normalize_float_categorical(X[:, i], min_clip, max_clip)
     return X
 
 
@@ -212,7 +223,7 @@ def make_synthetic(
     missing=False,
     objects=True,
     seed=1,
-    noise_intercept=0.0625,
+    intercept_shift=-4.0,  # avg y response is positive so shift to slighly negative
     noise_scale=1.0,
     cat_digits=4,
     min_clip=-2.0,
@@ -231,32 +242,32 @@ def make_synthetic(
     X_imp[missings] = 0.0
 
     # create some additive terms for our model to find
-    y = rng.normal(noise_intercept, noise_scale, n_samples)
-    y += np.exp(X_imp[:, 0])
-    y += X_imp[:, 1] ** 2
-    y += X_imp[:, 2] ** 3
+    y = rng.normal(intercept_shift, noise_scale, n_samples)
+    y += X_imp[:, 0] ** 2
+    y += X_imp[:, 1] ** 3
+    y += np.exp(X_imp[:, 2])
     y += X_imp[:, 3]  # integers (use linear response)
-    y += X_imp[:, 4]  # integers (use linear response)
+    y += -X_imp[:, 4]  # integers (use linear response)
     y += np.sin(3.14159 * 2.0 / (max_clip - min_clip) * X_imp[:, 5]) * 5.0
     y += np.cos(3.14159 * 2.0 / (max_clip - min_clip) * X_imp[:, 6]) * 5.0
     y += X_imp[:, 7]  # integer interaction feature
 
-    # 3-way interaction
-    y += X_imp[:, 0] * X_imp[:, 1] * X_imp[:, 2]
+    # linear addition of low cardinality categorical
+    y += X_imp[:, -2]
+
+    # linear addition of high cardinality categorical
+    y += X_imp[:, -1]
 
     # pairs
     y += X_imp[:, 0] * X_imp[:, 1]
     y += X_imp[:, 1] * X_imp[:, 2]
 
-    # linear addition of high cardinality categorical
-    y += X_imp[:, -2]
-
-    # linear addition of low cardinality categorical
-    y += X_imp[:, -1]
+    # 3-way interaction
+    y += X_imp[:, 0] * X_imp[:, 1] * X_imp[:, 2]
 
     if classes is not None and classes != 0:
         if type(classes) == int:
-            classes = np.arange(classes, dtype=int)
+            classes = np.arange(classes)
         else:
             classes = np.array(classes)
 
@@ -267,13 +278,12 @@ def make_synthetic(
             prob = np.exp(y)
             prob = prob / (1.0 + prob)
 
-            randoms = rng.uniform(0.0, 1.0, n_samples)
-            y = (randoms < prob).astype(int)
+            y = (rng.uniform(0.0, 1.0, n_samples) < prob).astype(int)
 
             if 2 < n_classes:
                 # multiclass. To keep things simple randomly select classes above 0th
-                randoms = rng.integers(1, n_classes, n_samples)
-                y = np.where(y == 0, 0, randoms)
+                y = np.where(y == 0, 0, rng.integers(1, n_classes, n_samples))
+
         y = classes[y]
 
     return (X, y, names, types)
