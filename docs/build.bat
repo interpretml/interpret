a3aed939bbf77b3d8c312ac8da0b5a2a9b600666
Paul Koch <code@koch.ninja>
2024-01-03 22:54:07 -0800

    add redirects to the docs

--- docs/build.bat
+++ docs/build.bat
@@ -1,6 +1,8 @@
 jupyter-book build --warningiserror --keep-going .\interpret\
 SET RETURNCODE=%ERRORLEVEL%
 
+copy .\extras\* .\interpret\_build\html\ /Y
+
 echo This file stops Github Pages from deleting directories that start with _> .\interpret\_build\html\.nojekyll
 
 exit /b %RETURNCODE%
--- docs/build.sh
+++ docs/build.sh
@@ -1,6 +1,8 @@
 jupyter-book build --warningiserror --keep-going ./interpret/
 ret_code=$?
 
+cp ./extras/* ./interpret/_build/html/
+
 # make an empty .nojekyll file because Github Pages will delete directories that start with '_' otherwise
 printf "This file stops Github Pages from deleting directories that start with _\n" > ./interpret/_build/html/.nojekyll
 
