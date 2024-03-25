interpretml/interpret

commit 01a5c3f89d9e8bb6d596d88edf09d0609845abc1

use the cached counts and weights instead of the computed ones

shared/libebm/ConvertAddBin.cpp
@@ -27,6 +27,8 @@ extern void ConvertAddBin(const size_t cScores,
       const bool bUInt64Src,
       const bool bDoubleSrc,
       const void* const aSrc,
+      const UIntMain* const aCounts,
+      const FloatPrecomp* const aWeights,
       const bool bUInt64Dest,
       const bool bDoubleDest,
       void* const aAddDest) {
@@ -291,28 +293,47 @@ extern void ConvertAddBin(const size_t cScores,
 
    EBM_ASSERT(0 <= iSrcHessian && 0 <= iDestHessian || iSrcHessian < 0 && iDestHessian < 0);
 
+   const UIntMain* pCounts = aCounts;
+   const FloatPrecomp* pWeights = aWeights;
+
    const unsigned char* pSrc = reinterpret_cast<const unsigned char*>(aSrc);
    const unsigned char* const pSrcEnd = pSrc + cSrcBinBytes * cBins;
    unsigned char* pAddDest = reinterpret_cast<unsigned char*>(aAddDest);
    const size_t cSrcArrayTotalBytes = cSrcArrayItemBytes * cScores;
    do {
-      if(bUInt64Src) {
+      if(nullptr != pCounts) {
+         const UIntMain src = *pCounts;
+         ++pCounts;
+         if(bUInt64Dest) {
+            *reinterpret_cast<uint64_t*>(pAddDest + iDestSamples) += static_cast<uint64_t>(src);
+         } else {
+            *reinterpret_cast<uint32_t*>(pAddDest + iDestSamples) += static_cast<uint32_t>(src);
+         }
+      } else if(bUInt64Src) {
          const uint64_t src = *reinterpret_cast<const uint64_t*>(pSrc + iSrcSamples);
          if(bUInt64Dest) {
-            *reinterpret_cast<uint64_t*>(pAddDest + iDestSamples) += src;
+            *reinterpret_cast<uint64_t*>(pAddDest + iDestSamples) += static_cast<uint64_t>(src);
          } else {
             *reinterpret_cast<uint32_t*>(pAddDest + iDestSamples) += static_cast<uint32_t>(src);
          }
       } else {
          const uint32_t src = *reinterpret_cast<const uint32_t*>(pSrc + iSrcSamples);
          if(bUInt64Dest) {
-            *reinterpret_cast<uint64_t*>(pAddDest + iDestSamples) += src;
+            *reinterpret_cast<uint64_t*>(pAddDest + iDestSamples) += static_cast<uint64_t>(src);
          } else {
-            *reinterpret_cast<uint32_t*>(pAddDest + iDestSamples) += src;
+            *reinterpret_cast<uint32_t*>(pAddDest + iDestSamples) += static_cast<uint32_t>(src);
          }
       }
 
-      if(bDoubleSrc) {
+      if(nullptr != pWeights) {
+         const FloatPrecomp src = *pWeights;
+         ++pWeights;
+         if(bDoubleDest) {
+            *reinterpret_cast<double*>(pAddDest + iDestWeight) += static_cast<double>(src);
+         } else {
+            *reinterpret_cast<float*>(pAddDest + iDestWeight) += static_cast<float>(src);
+         }
+      } else if(bDoubleSrc) {
          const double src = *reinterpret_cast<const double*>(pSrc + iSrcWeight);
          if(bDoubleDest) {
             *reinterpret_cast<double*>(pAddDest + iDestWeight) += static_cast<double>(src);

shared/libebm/GenerateTermUpdate.cpp
@@ -40,6 +40,8 @@ extern void ConvertAddBin(const size_t cScores,
       const bool bUInt64Src,
       const bool bDoubleSrc,
       const void* const aSrc,
+      const UIntMain* const aCounts,
+      const FloatPrecomp* const aWeights,
       const bool bUInt64Dest,
       const bool bDoubleDest,
       void* const aAddDest);
@@ -804,6 +806,10 @@ EBM_API_BODY ErrorEbm EBM_CALLING_CONVENTION GenerateTermUpdate(void* rng,
                   sizeof(UIntBig) == pSubset->GetObjectiveWrapper()->m_cUIntBytes,
                   sizeof(FloatBig) == pSubset->GetObjectiveWrapper()->m_cFloatBytes,
                   aFastBins,
+                  TermInnerBag::GetCounts(
+                        size_t{1} == cTensorBins, iTerm, iBag, pBoosterCore->GetTrainingSet()->GetTermInnerBags()),
+                  TermInnerBag::GetWeights(
+                        size_t{1} == cTensorBins, iTerm, iBag, pBoosterCore->GetTrainingSet()->GetTermInnerBags()),
                   std::is_same<UIntMain, uint64_t>::value,
                   std::is_same<FloatMain, double>::value,
                   aMainBins);

shared/libebm/CalcInteractionStrength.cpp
@@ -34,6 +34,8 @@ extern void ConvertAddBin(const size_t cScores,
       const bool bUInt64Src,
       const bool bDoubleSrc,
       const void* const aSrc,
+      const UIntMain* const aCounts,
+      const FloatPrecomp* const aWeights,
       const bool bUInt64Dest,
       const bool bDoubleDest,
       void* const aAddDest);
@@ -380,6 +382,8 @@ EBM_API_BODY ErrorEbm EBM_CALLING_CONVENTION CalcInteractionStrength(Interaction
             sizeof(UIntBig) == pSubset->GetObjectiveWrapper()->m_cUIntBytes,
             sizeof(FloatBig) == pSubset->GetObjectiveWrapper()->m_cFloatBytes,
             aFastBins,
+            nullptr,
+            nullptr,
             std::is_same<UIntMain, uint64_t>::value,
             std::is_same<FloatMain, double>::value,
             aMainBins);

shared/libebm/DataSetBoosting.cpp
@@ -915,17 +915,14 @@ ErrorEbm DataSetBoosting::InitBags(void* const rng, const size_t cInnerBags, con
       size_t iTerm = 0;
       do {
          const Term* const pTerm = apTerms[iTerm];
-         UIntMain* const aCounts = TermInnerBag::GetCounts(iTerm, iBag, m_aaTermInnerBags);
-         FloatPrecomp* const aWeights = TermInnerBag::GetWeights(iTerm, iBag, m_aaTermInnerBags);      
 
-         if(1 == pTerm->GetCountTensorBins()) {
-            if(nullptr != aCounts) {
-               aCounts[0] = cIncludedSamples;
-            }
-            if(nullptr != aWeights) {
-               aWeights[0] = totalWeight;
-            }
-         } else {
+         *TermInnerBag::GetCounts(true, iTerm, iBag, m_aaTermInnerBags) = cIncludedSamples;
+         *TermInnerBag::GetWeights(true, iTerm, iBag, m_aaTermInnerBags) = totalWeight;
+
+         if(1 != pTerm->GetCountTensorBins()) {
+            UIntMain* const aCounts = TermInnerBag::GetCounts(false, iTerm, iBag, m_aaTermInnerBags);
+            FloatPrecomp* const aWeights = TermInnerBag::GetWeights(false, iTerm, iBag, m_aaTermInnerBags);
+
             pWeightFrom = m_aOriginalWeights;
             pOccurrencesFrom = aOccurrencesFrom;
             pSubset = m_aSubsets;

shared/libebm/DataSetBoosting.hpp
@@ -145,6 +145,10 @@ struct DataSetBoosting final {
       EBM_ASSERT(nullptr != m_aBagWeightTotals);
       return m_aBagWeightTotals[iBag];
    }
+   inline const TermInnerBag* const * GetTermInnerBags() {
+      EBM_ASSERT(nullptr != m_aaTermInnerBags);
+      return m_aaTermInnerBags;
+   }
 
  private:
    ErrorEbm InitGradHess(const bool bAllocateHessians, const size_t cScores);

shared/libebm/TermInnerBag.hpp
@@ -34,15 +34,35 @@ struct TermInnerBag final {
          const size_t cInnerBags);
    static void FreeTermInnerBags(const size_t cTerms, TermInnerBag** const aaTermInnerBags, const size_t cInnerBags);
 
-   static inline UIntMain* GetCounts(const size_t iTerm, const size_t iBag, TermInnerBag** const aaTermInnerBags) {
-      return aaTermInnerBags[iTerm][iBag].m_aCounts;
+   static inline const UIntMain* GetCounts(const bool bCollapsed,
+         const size_t iTerm,
+         const size_t iBag,
+         const TermInnerBag* const* const aaTermInnerBags) {
+      return bCollapsed ? &aaTermInnerBags[iTerm][iBag].collapsedCount :
+                                        aaTermInnerBags[iTerm][iBag].m_aCounts;
+   }
+   static inline UIntMain* GetCounts(
+         const bool bCollapsed, const size_t iTerm, const size_t iBag, TermInnerBag** const aaTermInnerBags) {
+      return bCollapsed ? &aaTermInnerBags[iTerm][iBag].collapsedCount :
+                                        aaTermInnerBags[iTerm][iBag].m_aCounts;
+   }
+   static inline const FloatPrecomp* GetWeights(const bool bCollapsed,
+         const size_t iTerm,
+         const size_t iBag,
+         const TermInnerBag* const* const aaTermInnerBags) {
+      return bCollapsed ? &aaTermInnerBags[iTerm][iBag].collapsedWeight :
+                                        aaTermInnerBags[iTerm][iBag].m_aWeights;
    }
    static inline FloatPrecomp* GetWeights(
-         const size_t iTerm, const size_t iBag, TermInnerBag** const aaTermInnerBags) {
-      return aaTermInnerBags[iTerm][iBag].m_aWeights;
+         const bool bCollapsed, const size_t iTerm, const size_t iBag, TermInnerBag** const aaTermInnerBags) {
+      return bCollapsed ? &aaTermInnerBags[iTerm][iBag].collapsedWeight :
+                                        aaTermInnerBags[iTerm][iBag].m_aWeights;
    }
 
  private:
+   UIntMain collapsedCount;
+   FloatPrecomp collapsedWeight;
+
    UIntMain* m_aCounts;
    FloatPrecomp* m_aWeights;
 };

shared/libebm/bridge/Bin.hpp
@@ -72,6 +72,8 @@ template<typename TFloat, typename TUInt, bool bHessian, size_t cCompilerScores>
          const bool,
          const bool,
          const void* const,
+         const uint64_t* const,
+         const double* const,
          const bool,
          const bool,
          void* const);

commit be4e231513102f04c26a4788cfe2390c7e40469e

fix bug that the cached counts and weights were being added multiple times if there were multiple subsets (bug introduced in commit 01a5c3f8 on 2024-03-24)

shared/libebm/ConvertAddBin.cpp
@@ -556,9 +556,15 @@ extern void ConvertAddBin(const size_t cScores,
          const UIntMain src = *pCounts;
          ++pCounts;
          if(bUInt64Dest) {
-            *reinterpret_cast<uint64_t*>(pAddDest + iDestSamples) += static_cast<uint64_t>(src);
+            // we should only use aCounts on the last call to ConvertAddBin since it's added
+            EBM_ASSERT(0 == *reinterpret_cast<uint64_t*>(pAddDest + iDestSamples)); 
+            // assign instead of add since this should only be called this was on the last iteration
+            *reinterpret_cast<uint64_t*>(pAddDest + iDestSamples) = static_cast<uint64_t>(src);
          } else {
-            *reinterpret_cast<uint32_t*>(pAddDest + iDestSamples) += static_cast<uint32_t>(src);
+            // we should only use aCounts on the last call to ConvertAddBin since it's added
+            EBM_ASSERT(0 == *reinterpret_cast<uint32_t*>(pAddDest + iDestSamples));
+            // assign instead of add since this should only be called this was on the last iteration
+            *reinterpret_cast<uint32_t*>(pAddDest + iDestSamples) = static_cast<uint32_t>(src);
          }
       } else if(0 <= iSrcSamples) {
          EBM_ASSERT(0 <= iDestSamples);
@@ -585,9 +591,15 @@ extern void ConvertAddBin(const size_t cScores,
          const FloatPrecomp src = *pWeights;
          ++pWeights;
          if(bDoubleDest) {
-            *reinterpret_cast<double*>(pAddDest + iDestWeight) += static_cast<double>(src);
+            // we should only use aWeights on the last call to ConvertAddBin since it's added
+            EBM_ASSERT(0 == *reinterpret_cast<double*>(pAddDest + iDestWeight));
+            // assign instead of add since this should only be called this was on the last iteration
+            *reinterpret_cast<double*>(pAddDest + iDestWeight) = static_cast<double>(src);
          } else {
-            *reinterpret_cast<float*>(pAddDest + iDestWeight) += static_cast<float>(src);
+            // we should only use aWeights on the last call to ConvertAddBin since it's added
+            EBM_ASSERT(0 == *reinterpret_cast<float*>(pAddDest + iDestWeight));
+            // assign instead of add since this should only be called this was on the last iteration
+            *reinterpret_cast<float*>(pAddDest + iDestWeight) = static_cast<float>(src);
          }
       } else if(0 <= iSrcWeight) {
          EBM_ASSERT(0 <= iDestWeight);

shared/libebm/GenerateTermUpdate.cpp
@@ -802,22 +802,36 @@ EBM_API_BODY ErrorEbm EBM_CALLING_CONVENTION GenerateTermUpdate(void* rng,
                return error;
             }
 
+            const bool bUInt64Src = sizeof(UIntBig) == pSubset->GetObjectiveWrapper()->m_cUIntBytes;
+            const bool bDoubleSrc = sizeof(FloatBig) == pSubset->GetObjectiveWrapper()->m_cFloatBytes;
+
+            ++pSubset;
+
+            const UIntMain* aCounts = nullptr;
+            const FloatPrecomp* aWeights = nullptr;
+            if(pSubsetsEnd == pSubset) {
+               // the aCounts and aWeights tensors contain the final counts and weights, so when calling
+               // ConvertAddBin we only want to call it once with these tensors since otherwise they
+               // would be added multiple times
+               aCounts = TermInnerBag::GetCounts(
+                     size_t{1} == cTensorBins, iTerm, iBag, pBoosterCore->GetTrainingSet()->GetTermInnerBags());
+               aWeights = TermInnerBag::GetWeights(
+                     size_t{1} == cTensorBins, iTerm, iBag, pBoosterCore->GetTrainingSet()->GetTermInnerBags());
+            }
+
             ConvertAddBin(cScores,
                   pBoosterCore->IsHessian(),
                   cTensorBins,
-                  sizeof(UIntBig) == pSubset->GetObjectiveWrapper()->m_cUIntBytes,
-                  sizeof(FloatBig) == pSubset->GetObjectiveWrapper()->m_cFloatBytes,
+                  bUInt64Src,
+                  bDoubleSrc,
                   false,
                   false,
                   aFastBins,
-                  TermInnerBag::GetCounts(
-                        size_t{1} == cTensorBins, iTerm, iBag, pBoosterCore->GetTrainingSet()->GetTermInnerBags()),
-                  TermInnerBag::GetWeights(
-                        size_t{1} == cTensorBins, iTerm, iBag, pBoosterCore->GetTrainingSet()->GetTermInnerBags()),
+                  aCounts,
+                  aWeights,
                   std::is_same<UIntMain, uint64_t>::value,
                   std::is_same<FloatMain, double>::value,
                   aMainBins);
-            ++pSubset;
          } while(pSubsetsEnd != pSubset);
 
          // TODO: we can exit here back to python to allow caller modification to our histograms