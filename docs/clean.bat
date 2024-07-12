094be3eaa1c6b3b04682883492822835556cbda4
Paul Koch <code@koch.ninja>
2024-07-12 13:39:36 -0700

    add powerlift benchmark to documentation

--- docs/clean.bat
+++ docs/clean.bat
@@ -13,3 +13,8 @@ for %%f in (*.ipynb) do (
     jupyter nbconvert --to notebook --inplace --ClearOutputPreprocessor.enabled=True --ClearMetadataPreprocessor.enabled=True "%%f"
 )
 popd
+pushd benchmarks
+for %%f in (*.ipynb) do (
+    jupyter nbconvert --to notebook --inplace --ClearOutputPreprocessor.enabled=True --ClearMetadataPreprocessor.enabled=True "%%f"
+)
+popd
