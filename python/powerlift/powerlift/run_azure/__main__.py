009c1833ee407c4f56e548c6a1b152eeaf48617a
Paul Koch <code@koch.ninja>
2024-09-08 12:06:42 -0700

    hardened container error handling

--- python/powerlift/powerlift/run_azure/__main__.py
+++ python/powerlift/powerlift/run_azure/__main__.py
@@ -16,27 +16,78 @@ def run_azure_process(
     batch_id,
 ):
     startup_script = """
+        self_delete() {
+            echo "Attempt to self-delete this container group. Exit code was $1."
+
+            curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+            exit_code=$?
+            if [ $exit_code -ne 0 ]; then
+                echo "Failed to install azure tools with exit code $exit_code. Attempting to delete anyway."
+            fi
+
+            SUBSCRIPTION_ID=${SUBSCRIPTION_ID}
+            RESOURCE_GROUP_NAME=${RESOURCE_GROUP_NAME}
+            CONTAINER_GROUP_NAME=${CONTAINER_GROUP_NAME}
+
+            echo "Waiting 10 mintues to allow inspection of the logs..."
+            sleep 600
+
+            retry_count=0
+            while true; do
+                echo "Logging into azure to delete this container."
+                az login --identity
+                exit_code=$?
+                if [ $exit_code -ne 0 ]; then
+                    echo "az login failed with exit code $exit_code."
+                fi
+
+                echo "Deleting the container."
+                az container delete --subscription $SUBSCRIPTION_ID --resource-group $RESOURCE_GROUP_NAME --name $CONTAINER_GROUP_NAME --yes
+                exit_code=$?
+                if [ $exit_code -ne 0 ]; then
+                    echo "az container delete failed with exit code $exit_code."
+                fi
+
+                echo "Waiting to be deleted..."
+                sleep 300
+
+                if [ $retry_count -ge 300 ]; then
+                    echo "Maximum number of retries reached. Cannot self-delete this container group."
+                    break
+                fi
+                retry_count=$((retry_count + 1))
+            done
+
+            exit $1  # failed to self-kill the container we are running this on.
+        }
+    
         is_updated=0
         if ! command -v psql >/dev/null 2>&1; then
             is_updated=1
             apt-get --yes update
-            if [ $? -ne 0 ]; then
-                exit 63
+            exit_code=$?
+            if [ $exit_code -ne 0 ]; then
+                echo "apt-get --yes update failed with exit code $exit_code."
+                self_delete $exit_code
             fi
             apt-get --yes install postgresql-client
-            if [ $? -ne 0 ]; then
-                exit 63
+            exit_code=$?
+            if [ $exit_code -ne 0 ]; then
+                echo "apt-get --yes install postgresql-client failed with exit code $exit_code."
+                self_delete $exit_code
             fi
         fi
         retry_count=0
         while true; do
             shell_install=$(psql "$DB_URL" -c "SELECT shell_install FROM Experiment WHERE id='$EXPERIMENT_ID' LIMIT 1;" -t -A)
-            if [ $? -eq 0 ]; then
+            exit_code=$?
+            if [ $exit_code -eq 0 ]; then
                 break
             fi
+            echo "psql failed with exit code $exit_code."
             if [ $retry_count -ge 300 ]; then
                 echo "Maximum number of retries reached. Command failed."
-                exit 67
+                self_delete $exit_code
             fi
             retry_count=$((retry_count + 1))
             echo "Sleeping."
@@ -47,26 +98,32 @@ def run_azure_process(
             if [ $is_updated -eq 0 ]; then
                 is_updated=1
                 apt-get --yes update
-                if [ $? -ne 0 ]; then
-                    exit 63
+                exit_code=$?
+                if [ $exit_code -ne 0 ]; then
+                    echo "apt-get --yes update failed with exit code $exit_code."
+                    self_delete $exit_code
                 fi
             fi
             cmd="apt-get --yes install $shell_install"
             eval $cmd
-            if [ $? -ne 0 ]; then
-                exit 63
+            exit_code=$?
+            if [ $exit_code -ne 0 ]; then
+                echo "apt-get --yes install shell_install failed with exit code $exit_code."
+                self_delete $exit_code
             fi
         fi
 
         retry_count=0
         while true; do
             filenames=$(psql "$DB_URL" -c "SELECT name FROM wheel WHERE experiment_id='$EXPERIMENT_ID';" -t -A)
-            if [ $? -eq 0 ]; then
+            exit_code=$?
+            if [ $exit_code -eq 0 ]; then
                 break
             fi
+            echo "psql failed with exit code $exit_code."
             if [ $retry_count -ge 300 ]; then
                 echo "Maximum number of retries reached. Command failed."
-                exit 67
+                self_delete $exit_code
             fi
             retry_count=$((retry_count + 1))
             echo "Sleeping."
@@ -79,12 +136,14 @@ def run_azure_process(
                 retry_count=0
                 while true; do
                     psql "$DB_URL" -c "COPY (SELECT embedded FROM wheel WHERE experiment_id='$EXPERIMENT_ID' AND name='$filename') TO STDOUT WITH BINARY;" > "$filename"
-                    if [ $? -eq 0 ]; then
+                    exit_code=$?
+                    if [ $exit_code -eq 0 ]; then
                         break
                     fi
+                    echo "psql failed with exit code $exit_code."
                     if [ $retry_count -ge 300 ]; then
                         echo "Maximum number of retries reached. Command failed."
-                        exit 67
+                        self_delete $exit_code
                     fi
                     retry_count=$((retry_count + 1))
                     echo "Sleeping."
@@ -93,8 +152,10 @@ def run_azure_process(
                 done
                 cmd="python -m pip install $filename"
                 eval $cmd
-                if [ $? -ne 0 ]; then
-                    exit 63
+                exit_code=$?
+                if [ $exit_code -ne 0 ]; then
+                    echo "python -m pip install filename failed with exit code $exit_code."
+                    self_delete $exit_code
                 fi
             done
         fi
@@ -102,12 +163,14 @@ def run_azure_process(
         retry_count=0
         while true; do
             pip_install=$(psql "$DB_URL" -c "SELECT pip_install FROM Experiment WHERE id='$EXPERIMENT_ID' LIMIT 1;" -t -A)
-            if [ $? -eq 0 ]; then
+            exit_code=$?
+            if [ $exit_code -eq 0 ]; then
                 break
             fi
+            echo "psql failed with exit code $exit_code."
             if [ $retry_count -ge 300 ]; then
                 echo "Maximum number of retries reached. Command failed."
-                exit 67
+                self_delete $exit_code
             fi
             retry_count=$((retry_count + 1))
             echo "Sleeping."
@@ -117,25 +180,31 @@ def run_azure_process(
         if [ -n "$pip_install" ]; then
             cmd="python -m pip install $pip_install"
             eval $cmd
-            if [ $? -ne 0 ]; then
-                exit 63
+            exit_code=$?
+            if [ $exit_code -ne 0 ]; then
+                echo "python -m pip install pip_install failed with exit code $exit_code."
+                self_delete $exit_code
             fi
         fi
 
         python -m pip install psycopg2-binary powerlift
-        if [ $? -ne 0 ]; then
-            exit 63
+        exit_code=$?
+        if [ $exit_code -ne 0 ]; then
+            echo "python -m pip install psycopg2-binary powerlift failed with exit code $exit_code."
+            self_delete $exit_code
         fi
 
         retry_count=0
         while true; do
             result=$(psql "$DB_URL" -c "SELECT script FROM Experiment WHERE id='$EXPERIMENT_ID' LIMIT 1;" -t -A)
-            if [ $? -eq 0 ]; then
+            exit_code=$?
+            if [ $exit_code -eq 0 ]; then
                 break
             fi
+            echo "psql failed with exit code $exit_code."
             if [ $retry_count -ge 300 ]; then
                 echo "Maximum number of retries reached. Command failed."
-                exit 67
+                self_delete $exit_code
             fi
             retry_count=$((retry_count + 1))
             echo "Sleeping."
@@ -145,19 +214,21 @@ def run_azure_process(
         printf "%s" "$result" > "startup.py"
         echo "Running startup.py"
         python startup.py
-        exit_code=$?
-        echo "Powerlift startup.py script exited with code: $exit_code"
+        python_exit_code=$?
+        echo "Powerlift startup.py script exited with code: $python_exit_code"
         
-        if [ $exit_code -ne 0 ]; then
+        if [ $python_exit_code -ne 0 ]; then
             retry_count=0
             while true; do
                 result=$(psql "$DB_URL" -c "SELECT id FROM trial WHERE experiment_id='$EXPERIMENT_ID' AND runner_id='$RUNNER_ID' LIMIT 1;" -t -A)
-                if [ $? -eq 0 ]; then
+                exit_code=$?
+                if [ $exit_code -eq 0 ]; then
                     break
                 fi
+                echo "psql failed with exit code $exit_code."
                 if [ $retry_count -ge 300 ]; then
                     echo "Maximum number of retries reached. Command failed."
-                    exit 67
+                    self_delete $python_exit_code
                 fi
                 retry_count=$((retry_count + 1))
                 echo "Sleeping."
@@ -170,13 +241,15 @@ def run_azure_process(
 
                 retry_count=0
                 while true; do
-                    psql "$DB_URL" -c "UPDATE trial SET end_time=CURRENT_TIMESTAMP, errmsg='TERMINATED WITH ERROR CODE: $exit_code', runner_id=NULL WHERE id=$result AND errmsg is NULL;"
-                    if [ $? -eq 0 ]; then
+                    psql "$DB_URL" -c "UPDATE trial SET end_time=CURRENT_TIMESTAMP, errmsg='TERMINATED WITH ERROR CODE: $python_exit_code', runner_id=NULL WHERE id=$result AND errmsg is NULL;"
+                    exit_code=$?
+                    if [ $exit_code -eq 0 ]; then
                         break
                     fi
+                    echo "psql failed with exit code $exit_code."
                     if [ $retry_count -ge 300 ]; then
                         echo "Maximum number of retries reached. Command failed."
-                        exit 67
+                        self_delete $python_exit_code
                     fi
                     retry_count=$((retry_count + 1))
                     echo "Sleeping."
@@ -184,36 +257,9 @@ def run_azure_process(
                     echo "Retrying."
                 done
             fi
-
-            # Do not delete the container so that the log can be inspected.
-            # TODO: in the future try re-starting the python process to continue working on a new trial
-            exit $exit_code
         fi
 
-        # Attempt to self-delete the container group that we are running in.
-        curl -sL https://aka.ms/InstallAzureCLIDeb | bash
-
-        SUBSCRIPTION_ID=${SUBSCRIPTION_ID}
-        RESOURCE_GROUP_NAME=${RESOURCE_GROUP_NAME}
-        CONTAINER_GROUP_NAME=${CONTAINER_GROUP_NAME}
-
-        # wait 10 mintues to allow inspection of the logs
-        sleep 600
-
-        retry_count=0
-        while true; do
-            echo "Deleting this container."
-            az login --identity
-            az container delete --subscription $SUBSCRIPTION_ID --resource-group $RESOURCE_GROUP_NAME --name $CONTAINER_GROUP_NAME --yes
-            echo "Waiting to be deleted."
-            sleep 300
-            if [ $retry_count -ge 300 ]; then
-                echo "Maximum number of retries reached. Cannot self-delete this container group."
-                break
-            fi
-            retry_count=$((retry_count + 1))
-        done
-        exit 61  # failed to self-kill the container we are running this on.
+        self_delete $python_exit_code
     """
 
     import time
