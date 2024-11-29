7ba6c45dda54df436a619a2647518d3a9a7491a8
Paul Koch <code@koch.ninja>
2024-11-28 17:59:12 -0800

    rename partition straight files

--- build.sh
+++ build.sh
@@ -342,7 +342,7 @@ if [ $is_conda -eq 1 ]; then
    ${CXX} -c ${CPPFLAGS} ${CXXFLAGS} ${extras} "$code_path/PartitionOneDimensionalBoosting.cpp" -o "$tmp_path/PartitionOneDimensionalBoosting.o"
    ${CXX} -c ${CPPFLAGS} ${CXXFLAGS} ${extras} "$code_path/PartitionRandomBoosting.cpp" -o "$tmp_path/PartitionRandomBoosting.o"
    ${CXX} -c ${CPPFLAGS} ${CXXFLAGS} ${extras} "$code_path/PartitionMultiDimensionalTree.cpp" -o "$tmp_path/PartitionMultiDimensionalTree.o"
-   ${CXX} -c ${CPPFLAGS} ${CXXFLAGS} ${extras} "$code_path/PartitionTwoDimensionalInteraction.cpp" -o "$tmp_path/PartitionTwoDimensionalInteraction.o"
+   ${CXX} -c ${CPPFLAGS} ${CXXFLAGS} ${extras} "$code_path/PartitionMultiDimensionalStraight.cpp" -o "$tmp_path/PartitionMultiDimensionalStraight.o"
    ${CXX} -c ${CPPFLAGS} ${CXXFLAGS} ${extras} "$code_path/Purify.cpp" -o "$tmp_path/Purify.o"
    ${CXX} -c ${CPPFLAGS} ${CXXFLAGS} ${extras} "$code_path/RandomDeterministic.cpp" -o "$tmp_path/RandomDeterministic.o"
    ${CXX} -c ${CPPFLAGS} ${CXXFLAGS} ${extras} "$code_path/random.cpp" -o "$tmp_path/random.o"
@@ -380,7 +380,7 @@ if [ $is_conda -eq 1 ]; then
    "$tmp_path/PartitionOneDimensionalBoosting.o" \
    "$tmp_path/PartitionRandomBoosting.o" \
    "$tmp_path/PartitionMultiDimensionalTree.o" \
-   "$tmp_path/PartitionTwoDimensionalInteraction.o" \
+   "$tmp_path/PartitionMultiDimensionalStraight.o" \
    "$tmp_path/Purify.o" \
    "$tmp_path/RandomDeterministic.o" \
    "$tmp_path/random.o" \
