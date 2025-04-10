f37491567f5eb93111d54e38aeaca1ff3444ce27
Paul Koch <code@koch.ninja>
2025-04-09 23:32:29 -0700

    change seed

--- python/interpret-core/tests/glassbox/ebm/test_ebm_exact.py
+++ python/interpret-core/tests/glassbox/ebm/test_ebm_exact.py
@@ -65,6 +65,7 @@ def test_identical_ebm():
                 # in the least significant bit, so probably it's a floating point
                 # handling difference in the assembly code.
                 ebm.early_stopping_rounds = 0
+                ebm.random_state += 10000
 
             ebm.fit(X, y)
 
@@ -77,7 +78,7 @@ def test_identical_ebm():
 
             seed += 1
 
-    expected = 8.231780387209846e16
+    expected = 2.3818717018565084e+16
 
     print(n_rounds)
     print(interactions)
